0a65e23e3aead7caea3d3a35fa917da921257dc6
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2014-11-01 15:38:01 -0700

    Squashed 'third_party/src/github.com/GeertJohan/go.linenoise/' content from commit 5d5d527
    git-subtree-dir: third_party/src/github.com/GeertJohan/go.linenoise
    git-subtree-split: 5d5d5277d975f40b12167b478c0d79df00bda5a8

---
+++ windows.go
@@ -0,0 +1,8 @@
+package linenoise
+
+// #include <stdlib.h>
+// #include "linenoise.h"
+import "C"
+
+// this will break a go build on windows and display the string on console
+"go.linenoise does not support windows."
