e58cada88cfda00959308c7e133e8a26129b7205
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2014-11-01 21:05:40 -0700

    miniosd is now an http server, responds with hello world at root.

---
+++ server.go
@@ -0,0 +1,21 @@
+package minios
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type Server struct {
+}
+
+func (server *Server) Start() error {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HelloHandler)
+	fmt.Println("Running http server on port 8080")
+	return http.ListenAndServe(":8080", r)
+}
+
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Host: "+req.Host)
+}
