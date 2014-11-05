1e7515a7df7cfbead60fb9c7afb8660f2bf255b4
Harshavardhana <harsha@harshavardhana.net>
2014-11-05 03:09:40 -0800

    Add erasure coding and decoding using Intel Storage Acceleration library
     - move contrib/erasure --> contrib/isal
     - bring in low level 'isal' package for Go for exposing C functions
     - Implement Erasure 'encoding'
       Supports - Reed Solomon Codes, Cauchy Codes
     - Implement Erasure 'decoding'
       Supports - Reed Solomon Codes, Cauchy Codes
     - Renames Minios -> Minio at all the references

---
+++ erasure/cauchy_test.go
@@ -0,0 +1,63 @@
+/*
+ * Mini Object Storage, (C) 2014 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package erasure
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCachyEncode(t *testing.T) {
+	ep, _ := ValidateParams(10, 5, 8, CAUCHY)
+	p := NewEncoder(ep)
+
+	data := make([]byte, 1000)
+	chunks, length := p.Encode(data)
+
+	t.Logf("chunks length: %d;\nlength: %d\n", len(chunks), length)
+	if length != len(data) {
+		t.Fatal()
+	}
+}
+
+func TestCauchyDecode(t *testing.T) {
+	ep, _ := ValidateParams(10, 5, 8, CAUCHY)
+
+	data := []byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+
+	p := NewEncoder(ep)
+	chunks, length := p.Encode(data)
+	t.Logf("chunks length: %d;\nlength: %d\n", len(chunks), length)
+	if length != len(data) {
+		t.Fatal()
+	}
+
+	chunks[0] = nil
+	chunks[3] = nil
+	chunks[5] = nil
+	chunks[9] = nil
+	chunks[13] = nil
+
+	recovered_data, err := p.Decode(chunks, length)
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
+
+	if i := bytes.Compare(recovered_data, data); i < 0 {
+		t.Fatalf("Error: recovered data is less than original data")
+	}
+}
---
+++ erasure/ctypes.go
@@ -0,0 +1,58 @@
+/*
+ * Mini Object Storage, (C) 2014 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package erasure
+
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+// Integer to Int conversion
+func Int2cInt(src_err_list []int) *C.int {
+	SrcErrInt := int(unsafe.Sizeof(src_err_list[0]))
+	switch SrcErrInt {
+	case SizeInt:
+		return (*C.int)(unsafe.Pointer(&src_err_list[0]))
+	case SizeInt8:
+		Int8Array := make([]int8, len(src_err_list))
+		for i, v := range src_err_list {
+			Int8Array[i] = int8(v)
+		}
+		return (*C.int)(unsafe.Pointer(&Int8Array[0]))
+	case SizeInt16:
+		Int16Array := make([]int16, len(src_err_list))
+		for i, v := range src_err_list {
+			Int16Array[i] = int16(v)
+		}
+		return (*C.int)(unsafe.Pointer(&Int16Array[0]))
+	case SizeInt32:
+		Int32Array := make([]int32, len(src_err_list))
+		for i, v := range src_err_list {
+			Int32Array[i] = int32(v)
+		}
+		return (*C.int)(unsafe.Pointer(&Int32Array[0]))
+	case SizeInt64:
+		Int64Array := make([]int64, len(src_err_list))
+		for i, v := range src_err_list {
+			Int64Array[i] = int64(v)
+		}
+		return (*C.int)(unsafe.Pointer(&Int64Array[0]))
+	default:
+		panic(fmt.Sprintf("Unsupported: %d", SizeInt))
+	}
+}
