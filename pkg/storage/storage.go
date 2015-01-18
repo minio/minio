52fda4b08e6fc1a394de27a86939f5ffdab815b7
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2015-01-18 11:26:59 -0800

    Removing storage drivers

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,15 +1,1 @@
 package storage
-
-import "io"
-
-type ObjectStorage interface {
-	List(objectPath string) ([]ObjectDescription, error)
-	Get(path string) (io.Reader, error)
-	Put(path string, object io.Reader) error
-}
-
-type ObjectDescription struct {
-	Name    string
-	Md5sum  string
-	Murmur3 string
-}
