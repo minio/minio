8636e189e6c755638dfe827d9e911a9426597600
Frederick F. Kautz IV <fkautz@minio.io>
2015-01-22 16:19:55 -0800

    Cosmetic change, sorting routes

--- pkg/webapi/minioapi/minioapi.go
+++ pkg/webapi/minioapi/minioapi.go
@@ -42,13 +42,14 @@ func HttpHandler(storage mstorage.Storage) http.Handler {
 	api := minioApi{
 		storage: storage,
 	}
+
 	mux.HandleFunc("/", api.listBucketsHandler).Methods("GET")
-	mux.HandleFunc("/{bucket}", api.putBucketHandler).Methods("PUT")
 	mux.HandleFunc("/{bucket}", api.listObjectsHandler).Methods("GET")
+	mux.HandleFunc("/{bucket}", api.putBucketHandler).Methods("PUT")
 	mux.HandleFunc("/{bucket}/", api.listObjectsHandler).Methods("GET")
 	mux.HandleFunc("/{bucket}/{object:.*}", api.getObjectHandler).Methods("GET")
-	mux.HandleFunc("/{bucket}/{object:.*}", api.putObjectHandler).Methods("PUT")
 	mux.HandleFunc("/{bucket}/{object:.*}", api.headObjectHandler).Methods("HEAD")
+	mux.HandleFunc("/{bucket}/{object:.*}", api.putObjectHandler).Methods("PUT")
 	return mux
 }
 
