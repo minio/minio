74ef65daac4282d4b9aba7ddf856ffe17a37949d
Frederick F. Kautz IV <fkautz@minio.io>
2015-01-27 12:02:08 -0800

    Removing log from fs_test.go

--- pkg/storage/fs/fs_test.go
+++ pkg/storage/fs/fs_test.go
@@ -2,7 +2,6 @@ package fs
 
 import (
 	"io/ioutil"
-	"log"
 	"os"
 	"testing"
 
@@ -21,20 +20,16 @@ func (s *MySuite) TestAPISuite(c *C) {
 	var storageList []string
 	create := func() mstorage.Storage {
 		path, err := ioutil.TempDir(os.TempDir(), "minio-fs-")
-		log.Println(path)
 		c.Check(err, IsNil)
 		storageList = append(storageList, path)
 		_, _, store := Start(path)
 		return store
 	}
-	log.Println("FOO")
 	mstorage.APITestSuite(c, create)
-	log.Println("BAR")
 	removeRoots(c, storageList)
 }
 
 func removeRoots(c *C, roots []string) {
-	log.Println("REMOVING ROOTS: ", roots)
 	for _, root := range roots {
 		err := os.RemoveAll(root)
 		c.Check(err, IsNil)
