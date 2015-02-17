04075b15c9927db4e40b0cb192e3cda5f675611e
Anis Elleuch <vadmeste@gmail.com>
2015-02-17 19:17:31 +0100

    Fix Shebang position Add licence to setup_development_environment.sh

--- devscripts/checkdeps.sh
+++ devscripts/checkdeps.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #
 # Mini Object Storage, (C) 2015 Minio, Inc.
 #
@@ -13,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#!/usr/bin/env bash
 
 _init() {
     ## Minimum required versions for build dependencies
