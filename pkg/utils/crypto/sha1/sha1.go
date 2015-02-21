12cff1be5887c0aa3790dfd5569d1a05e29a0ad7
Harshavardhana <harsha@harshavardhana.net>
2015-02-21 11:25:09 -0800

    Implement sha1 as intel optimized set

--- pkg/utils/crypto/sha1/sha1.go
+++ pkg/utils/crypto/sha1/sha1.go
@@ -1,83 +1,150 @@
-/*
- * Mini Object Storage, (C) 2014 Minio, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Package sha1 implements the SHA1 hash algorithm as defined in RFC 3174.
 package sha1
 
-// #include <stdio.h>
-// #include <stdint.h>
-// void sha1_transform(int32_t *hash, const char* input, size_t num_blocks);
-import "C"
 import (
-	gosha1 "crypto/sha1"
+	"hash"
 	"io"
+
+	"github.com/minio-io/minio/pkg/utils/cpu"
 )
 
-/*
+// The size of a SHA1 checksum in bytes.
+const Size = 20
+
+// The blocksize of SHA1 in bytes.
+const BlockSize = 64
+
 const (
-	SHA1_BLOCKSIZE  = 64
-	SHA1_DIGESTSIZE = 20
+	chunk = 64
+	init0 = 0x67452301
+	init1 = 0xEFCDAB89
+	init2 = 0x98BADCFE
+	init3 = 0x10325476
+	init4 = 0xC3D2E1F0
 )
 
+// digest represents the partial evaluation of a checksum.
+type digest struct {
+	h   [5]uint32
+	x   [chunk]byte
+	nx  int
+	len uint64
+}
 
-func Sha1(buffer []byte) ([]int32, error) {
-	if cpu.HasAVX2() {
-		var shbuf []int32
-		var cbuffer *C.char
+func (d *digest) Reset() {
+	d.h[0] = init0
+	d.h[1] = init1
+	d.h[2] = init2
+	d.h[3] = init3
+	d.h[4] = init4
+	d.nx = 0
+	d.len = 0
+}
 
-		shbuf = make([]int32, SHA1_DIGESTSIZE)
-		var length = len(buffer)
-		if length == 0 {
-			return []int32{0}, errors.New("Invalid input")
-		}
+// New returns a new hash.Hash computing the SHA1 checksum.
+func New() hash.Hash {
+	d := new(digest)
+	d.Reset()
+	return d
+}
 
-		rem := length % SHA1_BLOCKSIZE
-		padded_len := length
-
-		if rem > 0 {
-			padded_len = length + (SHA1_BLOCKSIZE - rem)
-		}
-
-		rounds := padded_len / SHA1_BLOCKSIZE
-		pad := padded_len - length
-		if pad > 0 {
-			s := make([]byte, pad)
-			// Expand with new padded blocks to the byte array
-			buffer = append(buffer, s...)
-		}
-
-		cshbuf := (*C.int32_t)(unsafe.Pointer(&shbuf[0]))
-		cbuffer = (*C.char)(unsafe.Pointer(&buffer[0]))
-		C.sha1_transform(cshbuf, cbuffer, C.size_t(rounds))
-
-		return 0, nil
+func block(dig *digest, p []byte) {
+	switch true {
+	case cpu.HasAVX2() == true:
+		blockAVX2(dig, p)
+	default:
+		blockGeneric(dig, p)
 	}
 }
-*/
+
+func (d *digest) Size() int { return Size }
+
+func (d *digest) BlockSize() int { return BlockSize }
+
+func (d *digest) Write(p []byte) (nn int, err error) {
+	nn = len(p)
+	d.len += uint64(nn)
+	if d.nx > 0 {
+		n := copy(d.x[d.nx:], p)
+		d.nx += n
+		if d.nx == chunk {
+			block(d, d.x[:])
+			d.nx = 0
+		}
+		p = p[n:]
+	}
+	if len(p) >= chunk {
+		n := len(p) &^ (chunk - 1)
+		block(d, p[:n])
+		p = p[n:]
+	}
+	if len(p) > 0 {
+		d.nx = copy(d.x[:], p)
+	}
+	return
+}
+
+func (d0 *digest) Sum(in []byte) []byte {
+	// Make a copy of d0 so that caller can keep writing and summing.
+	d := *d0
+	hash := d.checkSum()
+	return append(in, hash[:]...)
+}
+
+func (d *digest) checkSum() [Size]byte {
+	len := d.len
+	// Padding.  Add a 1 bit and 0 bits until 56 bytes mod 64.
+	var tmp [64]byte
+	tmp[0] = 0x80
+	if len%64 < 56 {
+		d.Write(tmp[0 : 56-len%64])
+	} else {
+		d.Write(tmp[0 : 64+56-len%64])
+	}
+
+	// Length in bits.
+	len <<= 3
+	for i := uint(0); i < 8; i++ {
+		tmp[i] = byte(len >> (56 - 8*i))
+	}
+	d.Write(tmp[0:8])
+
+	if d.nx != 0 {
+		panic("d.nx != 0")
+	}
+
+	var digest [Size]byte
+	for i, s := range d.h {
+		digest[i*4] = byte(s >> 24)
+		digest[i*4+1] = byte(s >> 16)
+		digest[i*4+2] = byte(s >> 8)
+		digest[i*4+3] = byte(s)
+	}
+
+	return digest
+}
+
+// Convenience functions
+
+func Sum1(data []byte) [Size]byte {
+	var d digest
+	d.Reset()
+	d.Write(data)
+	return d.checkSum()
+}
+
 func Sum(reader io.Reader) ([]byte, error) {
-	hash := gosha1.New()
+	h := New()
 	var err error
 	for err == nil {
 		length := 0
 		byteBuffer := make([]byte, 1024*1024)
 		length, err = reader.Read(byteBuffer)
 		byteBuffer = byteBuffer[0:length]
-		hash.Write(byteBuffer)
+		h.Write(byteBuffer)
 	}
 	if err != io.EOF {
 		return nil, err
 	}
-	return hash.Sum(nil), nil
+	return h.Sum(nil), nil
 }
