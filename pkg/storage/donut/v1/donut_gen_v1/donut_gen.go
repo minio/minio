c107e6da66cb12cdd598fd2fc1ea9af1c07b95b1
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2015-02-28 15:39:24 -0800

    Adding new donut frame

--- pkg/storage/donut/v1/donut_gen_v1/donut_gen.go
+++ pkg/storage/donut/v1/donut_gen_v1/donut_gen.go
@@ -1,61 +1,65 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"os"
-	"reflect"
+// +build ignore
+func main() {}
 
-	"github.com/minio-io/minio/pkg/storage/donut/v1"
-)
-
-func main() {
-	fmt.Println("--start")
-
-	file, err := os.OpenFile("newfile", os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		panic(err)
-	}
-	donut := v1.New(file)
-
-	gobHeader := v1.GobHeader{}
-	data := []byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-
-	dataBuffer := bytes.NewBuffer(data)
-	err = donut.Write(gobHeader, dataBuffer)
-	if err != nil {
-		panic(err)
-	}
-	file.Close()
-	fmt.Println("--closed")
-
-	fmt.Println("--verify")
-	stat, _ := os.Stat("newfile")
-	fileSize := stat.Size()
-
-	rfile, _ := os.OpenFile("newfile", os.O_RDONLY, 0666)
-	blockStart := make([]byte, 4)
-	blockStartCheck := []byte{'M', 'I', 'N', 'I'}
-
-	_, err = rfile.Read(blockStart)
-	if err != nil {
-		panic(err)
-	}
-
-	blockEnd := make([]byte, 4)
-	start := fileSize - 4
-	blockEndCheck := []byte{'I', 'N', 'I', 'M'}
-	rfile.ReadAt(blockEnd, start)
-	rfile.Close()
-
-	if !reflect.DeepEqual(blockStart, blockStartCheck) {
-		panic("Corrupted donut file")
-	}
-
-	if !reflect.DeepEqual(blockEnd, blockEndCheck) {
-		panic("Corrupted donut file")
-	}
-
-	fmt.Println("--verified")
-	fmt.Println("--end")
-}
+//
+//import (
+//	"bytes"
+//	"fmt"
+//	"os"
+//	"reflect"
+//
+//	"github.com/minio-io/minio/pkg/storage/donut/v1"
+//)
+//
+//func main() {
+//	fmt.Println("--start")
+//
+//	file, err := os.OpenFile("newfile", os.O_WRONLY|os.O_CREATE, 0666)
+//	if err != nil {
+//		panic(err)
+//	}
+//	donut := v1.Write(file)
+//
+//	gobHeader := v1.GobHeader{}
+//	data := []byte("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+//
+//	dataBuffer := bytes.NewBuffer(data)
+//	err = donut.Write(gobHeader, dataBuffer)
+//	if err != nil {
+//		panic(err)
+//	}
+//	file.Close()
+//	fmt.Println("--closed")
+//
+//	fmt.Println("--verify")
+//	stat, _ := os.Stat("newfile")
+//	fileSize := stat.Size()
+//
+//	rfile, _ := os.OpenFile("newfile", os.O_RDONLY, 0666)
+//	blockStart := make([]byte, 4)
+//	blockStartCheck := []byte{'M', 'I', 'N', 'I'}
+//
+//	_, err = rfile.Read(blockStart)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	blockEnd := make([]byte, 4)
+//	start := fileSize - 4
+//	blockEndCheck := []byte{'I', 'N', 'I', 'M'}
+//	rfile.ReadAt(blockEnd, start)
+//	rfile.Close()
+//
+//	if !reflect.DeepEqual(blockStart, blockStartCheck) {
+//		panic("Corrupted donut file")
+//	}
+//
+//	if !reflect.DeepEqual(blockEnd, blockEndCheck) {
+//		panic("Corrupted donut file")
+//	}
+//
+//	fmt.Println("--verified")
+//	fmt.Println("--end")
+//}
--- pkg/storage/donut/v1/donut_test.go
+++ pkg/storage/donut/v1/donut_test.go
@@ -18,10 +18,14 @@ package v1
 
 import (
 	"bytes"
-	. "gopkg.in/check.v1"
-	"io/ioutil"
-	"os"
+	"crypto/sha512"
+	"encoding/binary"
+	"log"
 	"testing"
+
+	"github.com/minio-io/minio/pkg/utils/checksum/crc32c"
+
+	. "gopkg.in/check.v1"
 )
 
 func Test(t *testing.T) { TestingT(t) }
@@ -32,33 +36,97 @@ var _ = Suite(&MySuite{})
 
 func (s *MySuite) TestSingleWrite(c *C) {
 	//var b io.ReadWriteSeeker
-	var o bytes.Buffer
+	var testBuffer bytes.Buffer
 
-	b, err := ioutil.TempFile(os.TempDir(), "minio-donut-test")
-	defer os.RemoveAll(b.Name())
+	testData := "Hello, World"
+	testLength := uint64(len(testData))
+	err := Write(&testBuffer, bytes.NewBufferString(testData), testLength)
 	c.Assert(err, IsNil)
 
-	donut := New(b)
-	gobheader := GobHeader{}
-	err = donut.Write(gobheader, &o)
+	testBufferLength := uint64(testBuffer.Len())
+	log.Println(testBuffer.Bytes())
+
+	// we test our crc here too
+	headerBytes := testBuffer.Bytes()[0:28]
+	expectedCrc, err := crc32c.Crc32c(headerBytes)
 	c.Assert(err, IsNil)
-	blockStart := make([]byte, 4)
 
-	//n, _ := b.Read(blockStart)
-	// b.Next(b.Len() - n) // jump ahead
-	// b.Read(blockEnd)
-	// read start
-	b.Seek(0, 0) // jump ahead
-	b.Read(blockStart)
-	blockStartCheck := []byte{'M', 'I', 'N', 'I'}
-	c.Assert(blockStart, DeepEquals, blockStartCheck)
+	// magic mini
+	magicMini := make([]byte, 4)
+	testBuffer.Read(magicMini)
+	c.Assert(magicMini, DeepEquals, []byte{'M', 'I', 'N', 'I'})
 
-	// read block
+	// major version
+	majorVersion := make([]byte, 2)
+	testBuffer.Read(majorVersion)
+	c.Assert(binary.LittleEndian.Uint16(majorVersion), DeepEquals, uint16(1))
 
-	// read end
-	blockEnd := make([]byte, 4)
-	b.Seek(-int64(len(blockEnd)), 2) // jump ahead
-	b.Read(blockEnd)
-	blockEndCheck := []byte{'I', 'N', 'I', 'M'}
-	c.Assert(blockEnd, DeepEquals, blockEndCheck)
+	// minor version
+	minorVersion := make([]byte, 2)
+	testBuffer.Read(minorVersion)
+	c.Assert(binary.LittleEndian.Uint16(minorVersion), DeepEquals, uint16(0))
+
+	// patch version
+	patchVersion := make([]byte, 2)
+	testBuffer.Read(patchVersion)
+	c.Assert(binary.LittleEndian.Uint16(patchVersion), DeepEquals, uint16(0))
+
+	// reserved version
+	reservedVersion := make([]byte, 2)
+	testBuffer.Read(reservedVersion)
+	c.Assert(binary.LittleEndian.Uint16(reservedVersion), DeepEquals, uint16(0))
+
+	// reserved
+	reserved := make([]byte, 8)
+	testBuffer.Read(reserved)
+	c.Assert(binary.LittleEndian.Uint64(reserved), DeepEquals, uint64(0))
+
+	// data length
+	length := make([]byte, 8)
+	testBuffer.Read(length)
+	c.Assert(binary.LittleEndian.Uint64(length), DeepEquals, testLength)
+
+	// test crc
+	bufCrc := make([]byte, 4)
+	testBuffer.Read(bufCrc)
+	c.Assert(binary.LittleEndian.Uint32(bufCrc), DeepEquals, expectedCrc)
+
+	// magic DATA
+	magicData := make([]byte, 4)
+	testBuffer.Read(magicData)
+	c.Assert(magicData, DeepEquals, []byte{'D', 'A', 'T', 'A'})
+
+	// data
+	actualData := make([]byte, int32(testLength))
+	testBuffer.Read(actualData)
+	c.Assert(string(actualData), DeepEquals, testData)
+
+	// extract footer crc32c
+	actualFooterCrc := make([]byte, 4)
+	testBuffer.Read(actualFooterCrc)
+	remainingBytes := testBuffer.Bytes()
+	remainingSum, err := crc32c.Crc32c(remainingBytes)
+	c.Assert(err, IsNil)
+	c.Assert(binary.LittleEndian.Uint32(actualFooterCrc), DeepEquals, remainingSum)
+
+	// sha512
+	expectedSha512 := sha512.Sum512([]byte(testData))
+	actualSha512 := make([]byte, 64)
+	testBuffer.Read(actualSha512)
+	c.Assert(actualSha512, DeepEquals, expectedSha512[:])
+	log.Println("Length: ", testBuffer.Len())
+
+	// length
+	actualLength := make([]byte, 8)
+	testBuffer.Read(actualLength)
+	c.Assert(testBufferLength, DeepEquals, binary.LittleEndian.Uint64(actualLength))
+
+	// magic INIM
+	magicInim := make([]byte, 4)
+	testBuffer.Read(magicInim)
+	log.Println(magicInim)
+	c.Assert(magicInim, DeepEquals, []byte{'I', 'N', 'I', 'M'})
+
+	// ensure no extra data is in the file
+	c.Assert(testBuffer.Len(), Equals, 0)
 }
