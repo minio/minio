856b7680dfd9243194f429c07ea542fad42f28c1
Harshavardhana <harsha@harshavardhana.net>
2015-03-06 02:35:54 -0800

    Now able to use crc32c and sha1 on Darwin, with OSX specific code

---
+++ pkg/utils/crypto/sha1/sha1block_darwin.go
@@ -0,0 +1,13 @@
+// +build amd64
+
+package sha1
+
+// #include <stdint.h>
+// #include <stdlib.h>
+// void sha1_update_intel(int32_t *hash, const char* input, size_t num_blocks);
+import "C"
+import "unsafe"
+
+func blockSSE3(dig *digest, p []byte) {
+	C.sha1_update_intel((*C.int32_t)(unsafe.Pointer(&dig.h[0])), (*C.char)(unsafe.Pointer(&p[0])), (C.size_t)(len(p)/chunk))
+}
