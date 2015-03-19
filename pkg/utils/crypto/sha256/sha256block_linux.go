2b0b5f014a80c1e8115d81d6d435067ce23f7e7e
Harshavardhana <harsha@minio.io>
2015-03-19 14:35:50 -0700

    Update license header Mini should be Minimalist really

--- pkg/utils/crypto/sha256/sha256block_linux.go
+++ pkg/utils/crypto/sha256/sha256block_linux.go
@@ -1,7 +1,7 @@
 // +build amd64
 
 //
-// Mini Object Storage, (C) 2015 Minio, Inc.
+// Minimalist Object Storage, (C) 2015 Minio, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
