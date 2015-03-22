2fa9320df24d0d4d8ef5db208913c076c664a049
Harshavardhana <harsha@minio.io>
2015-03-22 15:14:06 -0700

    De-couple donut into smaller files, useful for ease in external integration

---
+++ pkg/storage/donut/donutwriter.go
@@ -0,0 +1,88 @@
+package donut
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+func newDonutFileWriter(objectDir string) (Writer, error) {
+	dataFile, err := os.OpenFile(path.Join(objectDir, "data"), os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return nil, err
+	}
+	return donutFileWriter{
+		root:          objectDir,
+		file:          dataFile,
+		metadata:      make(map[string]string),
+		donutMetadata: make(map[string]string),
+	}, nil
+}
+
+type donutFileWriter struct {
+	root          string
+	file          *os.File
+	metadata      map[string]string
+	donutMetadata map[string]string
+	err           error
+}
+
+func (d donutFileWriter) Write(data []byte) (int, error) {
+	return d.file.Write(data)
+}
+
+func (d donutFileWriter) Close() error {
+	if d.err != nil {
+		return d.err
+	}
+	metadata, _ := json.Marshal(d.metadata)
+	ioutil.WriteFile(path.Join(d.root, "metadata.json"), metadata, 0600)
+	donutMetadata, _ := json.Marshal(d.donutMetadata)
+	ioutil.WriteFile(path.Join(d.root, "donutMetadata.json"), donutMetadata, 0600)
+
+	return d.file.Close()
+}
+
+func (d donutFileWriter) CloseWithError(err error) error {
+	if d.err != nil {
+		d.err = err
+	}
+	return d.Close()
+}
+
+func (d donutFileWriter) SetMetadata(metadata map[string]string) error {
+	for k := range d.metadata {
+		delete(d.metadata, k)
+	}
+	for k, v := range metadata {
+		d.metadata[k] = v
+	}
+	return nil
+}
+
+func (d donutFileWriter) GetMetadata() (map[string]string, error) {
+	metadata := make(map[string]string)
+	for k, v := range d.metadata {
+		metadata[k] = v
+	}
+	return metadata, nil
+}
+
+func (d donutFileWriter) SetDonutMetadata(metadata map[string]string) error {
+	for k := range d.donutMetadata {
+		delete(d.donutMetadata, k)
+	}
+	for k, v := range metadata {
+		d.donutMetadata[k] = v
+	}
+	return nil
+}
+
+func (d donutFileWriter) GetDonutMetadata() (map[string]string, error) {
+	donutMetadata := make(map[string]string)
+	for k, v := range d.donutMetadata {
+		donutMetadata[k] = v
+	}
+	return donutMetadata, nil
+}
---
+++ pkg/storage/donut/local.go
@@ -0,0 +1,76 @@
+package donut
+
+import (
+	"errors"
+	"io"
+	"os"
+	"path"
+	"sort"
+	"strings"
+
+	"encoding/json"
+	"path/filepath"
+)
+
+type localDirectoryNode struct {
+	root string
+}
+
+func (d localDirectoryNode) GetBuckets() ([]string, error) {
+	return nil, errors.New("Not Implemented")
+}
+
+func (d localDirectoryNode) GetWriter(bucket, object string) (Writer, error) {
+	objectPath := path.Join(d.root, bucket, object)
+	err := os.MkdirAll(objectPath, 0700)
+	if err != nil {
+		return nil, err
+	}
+	return newDonutFileWriter(objectPath)
+}
+
+func (d localDirectoryNode) GetReader(bucket, object string) (io.ReadCloser, error) {
+	return os.Open(path.Join(d.root, bucket, object, "data"))
+}
+
+func (d localDirectoryNode) GetMetadata(bucket, object string) (map[string]string, error) {
+	return d.getMetadata(bucket, object, "metadata.json")
+}
+func (d localDirectoryNode) GetDonutMetadata(bucket, object string) (map[string]string, error) {
+	return d.getMetadata(bucket, object, "donutMetadata.json")
+}
+
+func (d localDirectoryNode) getMetadata(bucket, object, fileName string) (map[string]string, error) {
+	file, err := os.Open(path.Join(d.root, bucket, object, fileName))
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+	metadata := make(map[string]string)
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&metadata); err != nil {
+		return nil, err
+	}
+	return metadata, nil
+
+}
+
+func (d localDirectoryNode) ListObjects(bucketName string) ([]string, error) {
+	prefix := path.Join(d.root, bucketName)
+	var objects []string
+	if err := filepath.Walk(prefix, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && strings.HasSuffix(path, "data") {
+			object := strings.TrimPrefix(path, prefix+"/")
+			object = strings.TrimSuffix(object, "/data")
+			objects = append(objects, object)
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	sort.Strings(objects)
+	return objects, nil
+}
