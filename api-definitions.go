b07eed56be8b32374d80e3e81230af103482e381
Harshavardhana <harsha@harshavardhana.net>
2015-12-07 14:52:27 -0800

    accessPolicy: Access policy response is not correct.

--- api-definitions.go
+++ api-definitions.go
@@ -25,6 +25,8 @@ const (
 
 // AccessControlPolicyResponse - format for get bucket acl response
 type AccessControlPolicyResponse struct {
+	XMLName xml.Name `xml:"http://s3.amazonaws.com/doc/2006-03-01/ AccessControlPolicy" json:"-"`
+
 	AccessControlList struct {
 		Grant []Grant
 	}
