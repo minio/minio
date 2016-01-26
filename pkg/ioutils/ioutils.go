18375b7794a4c36f03bbd350a53b0a787ec41d8a
Harshavardhana <harsha@harshavardhana.net>
2016-01-26 12:34:04 -0800

    ioutils: Add tests

--- pkg/ioutils/ioutils.go
+++ pkg/ioutils/ioutils.go
@@ -1,3 +1,19 @@
+/*
+ * Minio Cloud Storage, (C) 2016 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ioutils
 
 import (
@@ -20,8 +36,10 @@ func ReadDirN(dirname string, n int) ([]os.FileInfo, error) {
 		return nil, err
 	}
 	list, err := f.Readdir(n)
-	f.Close()
 	if err != nil {
+		return nil, err
+	}
+	if err = f.Close(); err != nil {
 		return nil, err
 	}
 	sort.Sort(byName(list))
@@ -36,8 +54,10 @@ func ReadDirNamesN(dirname string, n int) ([]string, error) {
 		return nil, err
 	}
 	names, err := f.Readdirnames(n)
-	f.Close()
 	if err != nil {
+		return nil, err
+	}
+	if err = f.Close(); err != nil {
 		return nil, err
 	}
 	sort.Strings(names)
