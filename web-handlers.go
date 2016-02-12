9b3967a02fca078e2ad4891439a93294cfa914e6
Krishna Srinivas <krishna.srinivas@gmail.com>
2016-02-13 01:04:43 +0530

    WebUI: move from rpc/v2/json to rpc/v2/json2 which has better error response structure.

--- web-handlers.go
+++ web-handlers.go
@@ -17,7 +17,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
@@ -35,19 +34,6 @@ import (
 	"github.com/minio/minio/pkg/disk"
 	"github.com/minio/minio/pkg/probe"
 )
-
-type webError struct {
-	Message string `json:"message"`
-	Details string `json:"details"`
-}
-
-func (we webError) Error() string {
-	retStr, err := json.Marshal(we)
-	if err != nil {
-		return err.Error()
-	}
-	return string(retStr)
-}
 
 // isAuthenticated validates if any incoming request to be a valid JWT
 // authenticated request.
