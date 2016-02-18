f53e9dd1b8e163dad5bbdbe5fa72dfcc02240426
Anand Babu (AB) Periasamy <ab@minio.io>
2016-02-18 13:02:41 +0530

    vendor update for go-homedir

--- vendor/github.com/minio/go-homedir/homedir.go
+++ vendor/github.com/minio/go-homedir/homedir.go
@@ -1,14 +1,12 @@
+// Copyright 2016 (C) Mitchell Hashimoto
+// Distributed under the MIT License.
+
+// Package homedir implements a portable function to determine current user's homedir.
 package homedir
 
 import (
-	"bytes"
 	"errors"
-	"os"
-	"os/exec"
 	"path/filepath"
-	"runtime"
-	"strconv"
-	"strings"
 	"sync"
 )
 
@@ -17,37 +15,30 @@ import (
 var DisableCache bool
 
 var homedirCache string
-var cacheLock sync.RWMutex
+var cacheLock sync.Mutex
 
 // Dir returns the home directory for the executing user.
 //
 // This uses an OS-specific method for discovering the home directory.
 // An error is returned if a home directory cannot be detected.
 func Dir() (string, error) {
-	if !DisableCache {
-		cacheLock.RLock()
-		cached := homedirCache
-		cacheLock.RUnlock()
-		if cached != "" {
-			return cached, nil
-		}
-	}
-
 	cacheLock.Lock()
 	defer cacheLock.Unlock()
 
-	var result string
-	var err error
-	if runtime.GOOS == "windows" {
-		result, err = dirWindows()
-	} else {
-		// Unix-like system, so just assume Unix
-		result, err = dirUnix()
+	// Return cached homedir if available.
+	if !DisableCache {
+		if homedirCache != "" {
+			return homedirCache, nil
+		}
 	}
 
+	// Determine OS speific current homedir.
+	result, err := dir()
 	if err != nil {
 		return "", err
 	}
+
+	// Cache for future lookups.
 	homedirCache = result
 	return result, nil
 }
@@ -74,59 +65,4 @@ func Expand(path string) (string, error) {
 	}
 
 	return filepath.Join(dir, path[1:]), nil
-}
-
-func dirUnix() (string, error) {
-	// First prefer the HOME environmental variable
-	if home := os.Getenv("HOME"); home != "" {
-		return home, nil
-	}
-
-	// If that fails, try getent
-	var stdout bytes.Buffer
-	cmd := exec.Command("getent", "passwd", strconv.Itoa(os.Getuid()))
-	cmd.Stdout = &stdout
-	if err := cmd.Run(); err != nil {
-		// If "getent" is missing, ignore it
-		if err != exec.ErrNotFound {
-			return "", err
-		}
-	} else {
-		if passwd := strings.TrimSpace(stdout.String()); passwd != "" {
-			// username:password:uid:gid:gecos:home:shell
-			passwdParts := strings.SplitN(passwd, ":", 7)
-			if len(passwdParts) > 5 {
-				return passwdParts[5], nil
-			}
-		}
-	}
-
-	// If all else fails, try the shell
-	stdout.Reset()
-	cmd = exec.Command("sh", "-c", "cd && pwd")
-	cmd.Stdout = &stdout
-	if err := cmd.Run(); err != nil {
-		return "", err
-	}
-
-	result := strings.TrimSpace(stdout.String())
-	if result == "" {
-		return "", errors.New("blank output when reading home directory")
-	}
-
-	return result, nil
-}
-
-func dirWindows() (string, error) {
-	drive := os.Getenv("HOMEDRIVE")
-	path := os.Getenv("HOMEPATH")
-	home := drive + path
-	if drive == "" || path == "" {
-		home = os.Getenv("USERPROFILE")
-	}
-	if home == "" {
-		return "", errors.New("HOMEDRIVE, HOMEPATH, and USERPROFILE are blank")
-	}
-
-	return home, nil
 }
