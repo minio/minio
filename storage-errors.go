2b65fd99e2c173f89639812ac808bf2e5dc86b85
Harshavardhana <harsha@minio.io>
2016-04-20 17:23:23 -0700

    obj: Object api handle all errors in common location. (#1343)

--- storage-errors.go
+++ storage-errors.go
@@ -44,4 +44,7 @@ var errVolumeAccessDenied = errors.New("volume access denied")
 var errFileAccessDenied = errors.New("file access denied")
 
 // errReadQuorum - did not meet read quorum.
-var errReadQuorum = errors.New("I/O error.  do not meet read quorum")
+var errReadQuorum = errors.New("I/O error.  did not meet read quorum.")
+
+// errWriteQuorum - did not meet write quorum.
+var errWriteQuorum = errors.New("I/O error.  did not meet write quorum.")
