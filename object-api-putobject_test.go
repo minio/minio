c54a4b2cde7f305362e9f9338c91ea6ebc4ad8ee
Karthic Rao <karthic@minio.io>
2016-06-27 14:36:10 +0530

    Adding detailed comments for server_xl_test.

--- object-api-putobject_test.go
+++ object-api-putobject_test.go
@@ -120,7 +120,7 @@ func testObjectAPIPutObject(obj ObjectLayer, instanceType string, t *testing.T) 
 }
 
 // Wrapper for calling PutObject tests for both XL multiple disks case
-// when quorum is not availabel.
+// when quorum is not available.
 func TestObjectAPIPutObjectDiskNotFound(t *testing.T) {
 	ExecObjectLayerDiskNotFoundTest(t, testObjectAPIPutObjectDiskNotFOund)
 }
