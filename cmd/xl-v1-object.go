88023fa66b9cb87f5c55591c0f21268480a513cc
Krishnan Parthasarathi <kp@minio.io>
2016-09-22 16:47:05 -0700

    Purge stale object cache entry
    - Add unit test to validate that ModTime based cache entry purging works.

--- cmd/xl-v1-object.go
+++ cmd/xl-v1-object.go
@@ -121,7 +121,7 @@ func (xl xlObjects) GetObject(bucket, object string, startOffset int64, length i
 	if xlMeta.Stat.Size > 0 && xl.objCacheEnabled {
 		// Validate if we have previous cache.
 		var cachedBuffer io.ReadSeeker
-		cachedBuffer, err = xl.objCache.Open(path.Join(bucket, object))
+		cachedBuffer, err = xl.objCache.Open(path.Join(bucket, object), modTime)
 		if err == nil { // Cache hit.
 			// Advance the buffer to offset as if it was read.
 			if _, err = cachedBuffer.Seek(startOffset, 0); err != nil { // Seek to the offset.
