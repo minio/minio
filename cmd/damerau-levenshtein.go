52c6ee3c767e09bb5595f6fa75e170ca2956247b
Aditya Manthramurthy <aditya@minio.io>
2016-09-27 11:35:41 -0700

    Make copyright year and notice uniform

--- cmd/damerau-levenshtein.go
+++ cmd/damerau-levenshtein.go
@@ -1,5 +1,5 @@
 /*
- * Minio Client (C) 2014-2016 Minio, Inc.
+ * Minio Cloud Storage (C) 2014-2016 Minio, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- cmd/logger-syslog-hook_nix.go
+++ cmd/logger-syslog-hook_nix.go
@@ -1,7 +1,7 @@
 // +build !windows
 
 /*
- * Minio Cloud Storage, (C) 2015, 2016 Minio, Inc.
+ * Minio Cloud Storage (C) 2014-2016 Minio, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
