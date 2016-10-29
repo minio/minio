a016a9c340a4cb4dbd1eee834cb36d5104504a44
Krishna Srinivas <krishna.srinivas@gmail.com>
2016-10-29 14:26:39 +0530

    Cleanup - remove stackInfo, funcFromPC and use simpler callerLocation().

--- cmd/logger.go
+++ cmd/logger.go
@@ -17,13 +17,9 @@
 package cmd
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"path"
-	"path/filepath"
 	"runtime"
-	"runtime/debug"
 	"strings"
 
 	"github.com/Sirupsen/logrus"
@@ -44,37 +40,6 @@ type logger struct {
 	File    fileLogger    `json:"file"`
 	Syslog  syslogLogger  `json:"syslog"`
 	// Add new loggers here.
-}
-
-// Function takes input with the results from runtime.Caller(1). Depending on the boolean.
-// This function can either returned a shotFile form or a longFile form.
-func funcFromPC(pc uintptr, file string, line int, shortFile bool) string {
-	var fn, name string
-	if shortFile {
-		fn = strings.Replace(file, path.Join(filepath.ToSlash(GOPATH)+"/src/github.com/minio/minio/cmd/")+"/", "", -1)
-		name = strings.Replace(runtime.FuncForPC(pc).Name(), "github.com/minio/minio/cmd.", "", -1)
-	} else {
-		fn = strings.Replace(file, path.Join(filepath.ToSlash(GOPATH)+"/src/")+"/", "", -1)
-		name = strings.Replace(runtime.FuncForPC(pc).Name(), "github.com/minio/minio/cmd.", "", -1)
-	}
-	return fmt.Sprintf("%s [%s:%d]", name, fn, line)
-}
-
-// stackInfo returns printable stack trace.
-func stackInfo() string {
-	// Convert stack-trace bytes to io.Reader.
-	rawStack := bufio.NewReader(bytes.NewBuffer(debug.Stack()))
-	// Skip stack trace lines until our real caller.
-	for i := 0; i <= 4; i++ {
-		rawStack.ReadLine()
-	}
-
-	// Read the rest of useful stack trace.
-	stackBuf := new(bytes.Buffer)
-	stackBuf.ReadFrom(rawStack)
-
-	// Strip GOPATH of the build system and return.
-	return strings.Replace(stackBuf.String(), filepath.ToSlash(GOPATH)+"/src/", "", -1)
 }
 
 // Get file, line, function name of the caller.
