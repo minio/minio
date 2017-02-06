8a33b1a92d0dc58945ce61cc7084f2effc764204
Harshavardhana <harsha@minio.io>
2017-02-06 11:39:07 -0800

    [WIP] reduce the min prepare storage requirement.

--- cmd/prepare-storage-msg.go
+++ cmd/prepare-storage-msg.go
@@ -103,9 +103,7 @@ const (
 // healing is optional, server continues to initialize object layer after printing this message.
 // it is upto the end user to perform a heal if needed.
 func getHealMsg(endpoints []*url.URL, storageDisks []StorageAPI) string {
-	msg := fmt.Sprintln("\nData volume requires HEALING. Healing is not implemented yet stay tuned:")
-	// FIXME:  Enable this after we bring in healing.
-	//	msg := "mc admin heal myminio"
+	msg := fmt.Sprintln("\nData volume requires HEALING. Please use `mc admin heal <alias>` to heal the unformatted disks:")
 	disksInfo, _, _ := getDisksInfo(storageDisks)
 	for i, info := range disksInfo {
 		if storageDisks[i] == nil {
