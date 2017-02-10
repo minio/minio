8912b6bf3b22ff668b6448742cd50e7b837182ec
Bala FA <bala.gluster@gmail.com>
2017-02-11 01:21:41 +0530

    trie: new package (#3729)
    This implements a simple trie tree for minio server/tools.
    This package borrows idea from
    https://godoc.org/golang.org/x/text/internal/triegen.

--- pkg/trie/trie.go
+++ pkg/trie/trie.go
@@ -1,5 +1,5 @@
 /*
- * Minio Cloud Storage, (C) 2015 Minio, Inc.
+ * Minio Cloud Storage, (C) 2014, 2015, 2016, 2017 Minio, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,80 +14,78 @@
  * limitations under the License.
  */
 
-package cmd
+// Package trie implements a simple trie tree for minio server/tools borrows
+// idea from - https://godoc.org/golang.org/x/text/internal/triegen.
+package trie
 
-// This package borrows idea from - https://godoc.org/golang.org/x/text/internal/triegen
+// Node trie tree node container carries value and children.
+type Node struct {
+	exists bool
+	value  interface{}
+	child  map[rune]*Node // runes as child.
+}
 
-// Trie trie container
+// newNode create a new trie node.
+func newNode() *Node {
+	return &Node{
+		exists: false,
+		value:  nil,
+		child:  make(map[rune]*Node),
+	}
+}
+
+// Trie is a trie container.
 type Trie struct {
-	root *trieNode
+	root *Node
 	size int
 }
 
-// newTrie get new trie
-func newTrie() *Trie {
-	return &Trie{
-		root: newTrieNode(),
-		size: 0,
-	}
+// Root returns root node.
+func (t *Trie) Root() *Node {
+	return t.root
 }
 
-// trieNode trie tree node container carries value and children
-type trieNode struct {
-	exists bool
-	value  interface{}
-	child  map[rune]*trieNode // runes as child
-}
-
-func newTrieNode() *trieNode {
-	return &trieNode{
-		exists: false,
-		value:  nil,
-		child:  make(map[rune]*trieNode),
-	}
-}
-
-// Insert insert a key
+// Insert insert a key.
 func (t *Trie) Insert(key string) {
 	curNode := t.root
 	for _, v := range key {
 		if curNode.child[v] == nil {
-			curNode.child[v] = newTrieNode()
+			curNode.child[v] = newNode()
 		}
 		curNode = curNode.child[v]
 	}
 
 	if !curNode.exists {
-		// increment when new rune child is added
+		// increment when new rune child is added.
 		t.size++
 		curNode.exists = true
 	}
-	// value is stored for retrieval in future
+	// value is stored for retrieval in future.
 	curNode.value = key
 }
 
-// PrefixMatch - prefix match
+// PrefixMatch - prefix match.
 func (t *Trie) PrefixMatch(key string) []interface{} {
 	node, _ := t.findNode(key)
 	if node != nil {
-		return t.walk(node)
+		return t.Walk(node)
 	}
 	return []interface{}{}
 }
 
-// walk the tree
-func (t *Trie) walk(node *trieNode) (ret []interface{}) {
+// Walk the tree.
+func (t *Trie) Walk(node *Node) (ret []interface{}) {
 	if node.exists {
 		ret = append(ret, node.value)
 	}
 	for _, v := range node.child {
-		ret = append(ret, t.walk(v)...)
+		ret = append(ret, t.Walk(v)...)
 	}
 	return
 }
 
-// find nodes corresponding to key
-func (t *Trie) findNode(key string) (node *trieNode, index int) {
+// find nodes corresponding to key.
+func (t *Trie) findNode(key string) (node *Node, index int) {
 	curNode := t.root
 	f := false
 	for k, v := range key {
@@ -109,4 +107,12 @@ func (t *Trie) findNode(key string) (node *trieNode, index int) {
 	}
 
 	return curNode, index
+}
+
+// NewTrie create a new trie.
+func NewTrie() *Trie {
+	return &Trie{
+		root: newNode(),
+		size: 0,
+	}
 }
