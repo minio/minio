5327cc699e12b42af7b87aeec4fd27be66352bef
Harshavardhana <harsha@minio.io>
2017-02-15 02:51:19 -0800

    sRPC/client: Properly trim storageRPCPath for actual disk path.
    Never print internal RPC endpoint paths.

--- cmd/storage-rpc-client.go
+++ cmd/storage-rpc-client.go
@@ -23,6 +23,7 @@ import (
 	"net/rpc"
 	"net/url"
 	"path"
+	"strings"
 
 	"github.com/minio/minio/pkg/disk"
 )
@@ -132,20 +133,20 @@ func newStorageRPC(ep *url.URL) (StorageAPI, error) {
 
 // Stringer interface compatible representation of network device.
 func (n *networkStorage) String() string {
-	return n.rpcClient.ServerAddr() + ":" + n.rpcClient.ServiceEndpoint()
+	// Remove the storage RPC path prefix, internal paths are meaningless.
+	serviceEndpoint := strings.TrimPrefix(n.rpcClient.ServiceEndpoint(), storageRPCPath)
+	return n.rpcClient.ServerAddr() + ":" + serviceEndpoint
 }
 
 // Init - attempts a login to reconnect.
 func (n *networkStorage) Init() error {
-	err := n.rpcClient.Login()
-	return toStorageErr(err)
+	return toStorageErr(n.rpcClient.Login())
 }
 
 // Closes the underlying RPC connection.
-func (n *networkStorage) Close() (err error) {
+func (n *networkStorage) Close() error {
 	// Close the underlying connection.
-	err = n.rpcClient.Close()
-	return toStorageErr(err)
+	return toStorageErr(n.rpcClient.Close())
 }
 
 // DiskInfo - fetch disk information for a remote disk.
