10917342a259675ae286b5d49dbb60cfe8041fda
Harshavardhana <harsha@minio.io>
2017-02-28 16:00:10 -0800

    Revert "Fix races"
    This reverts commit 774eefade60d5a886205cc6a0ec666c89abcc355.

--- pkg/lock/lock.go
+++ pkg/lock/lock.go
@@ -20,7 +20,7 @@ package lock
 
 import (
 	"os"
-	"sync/atomic"
+	"sync"
 )
 
 // RLockedFile represents a read locked file, implements a special
@@ -28,31 +28,38 @@ import (
 // has reached zero, i.e when all the readers have given up their locks.
 type RLockedFile struct {
 	*LockedFile
-	refs int64 // Holds read lock refs.
+	mutex sync.Mutex
+	refs  int // Holds read lock refs.
 }
 
 // IsClosed - Check if the rlocked file is already closed.
 func (r *RLockedFile) IsClosed() bool {
-	return atomic.LoadInt64(&r.refs) == 0
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	return r.refs == 0
 }
 
 // IncLockRef - is used by called to indicate lock refs.
 func (r *RLockedFile) IncLockRef() {
-	atomic.AddInt64(&r.refs, 1)
+	r.mutex.Lock()
+	r.refs++
+	r.mutex.Unlock()
 }
 
 // Close - this closer implements a special closer
 // closes the underlying fd only when the refs
 // reach zero.
 func (r *RLockedFile) Close() (err error) {
-	refs := atomic.LoadInt64(&r.refs)
-	if refs == 0 {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if r.refs == 0 {
 		return os.ErrInvalid
 	}
 
-	refs = atomic.AddInt64(&r.refs, -1)
-	if refs == 0 {
-		err = r.LockedFile.Close()
+	r.refs--
+	if r.refs == 0 {
+		err = r.File.Close()
 	}
 
 	return err
