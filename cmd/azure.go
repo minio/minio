ca21628dbd8eb4fea6a8453358dd357c53f18cc1
Krishna Srinivas <krishna.srinivas@gmail.com>
2017-03-10 15:38:19 -0800

    Fix GET object of empty files.

--- cmd/azure.go
+++ cmd/azure.go
@@ -232,6 +232,10 @@ func (a AzureObjects) ListObjects(bucket, prefix, marker, delimiter string, maxK
 
 // GetObject - Use Azure equivalent GetBlobRange.
 func (a AzureObjects) GetObject(bucket, object string, startOffset int64, length int64, writer io.Writer) error {
+	if length == 0 {
+		_, err := a.GetObjectInfo(bucket, object)
+		return err
+	}
 	byteRange := fmt.Sprintf("%d-%d", startOffset, startOffset+length-1)
 
 	rc, err := a.client.GetBlobRange(bucket, object, byteRange, nil)
@@ -472,6 +476,10 @@ func (a AzureObjects) CompleteMultipartUpload(bucket, object, uploadID string, u
 // AnonGetObject - SendGET request without authentication.
 // This is needed when clients send GET requests on objects that can be downloaded without auth.
 func (a AzureObjects) AnonGetObject(bucket, object string, startOffset int64, length int64, writer io.Writer) (err error) {
+	if length == 0 {
+		_, err := a.AnonGetObjectInfo(bucket, object)
+		return err
+	}
 	url := a.client.GetBlobURL(bucket, object)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
