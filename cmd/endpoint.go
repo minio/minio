573e9d38a32bd52ef2a87cf6a2841fab570124c1
frankw <fwessels@xs4all.nl>
2017-04-21 16:05:47 -0700

    In lieu of expected relaxing of maxErasureBlocks, simplify error message when signalling back to the user (prevent a too long list).

--- cmd/endpoint.go
+++ cmd/endpoint.go
@@ -185,23 +185,9 @@ func NewEndpointList(args ...string) (endpoints EndpointList, err error) {
 		return (count >= minErasureBlocks && count <= maxErasureBlocks && count%2 == 0)
 	}
 
-	// getValidErasureBlocks returns a string containing the valid values for erasure coding.
-	getValidErasureBlocks := func() string {
-		list := ""
-		for blocks := minErasureBlocks; blocks <= maxErasureBlocks; blocks += 2 {
-			list += fmt.Sprintf("%d", blocks)
-			if blocks < maxErasureBlocks-2 {
-				list += ", "
-			} else if blocks == maxErasureBlocks-2 {
-				list += " or "
-			}
-		}
-		return list
-	}
-
 	// Check whether no. of args are valid for XL distribution.
 	if !isValidDistribution(len(args)) {
-		return nil, fmt.Errorf("A total of %d endpoints were found. For erasure mode it should be %s", len(args), getValidErasureBlocks())
+		return nil, fmt.Errorf("A total of %d endpoints were found. For erasure mode it should be an even number between %d and %d", len(args), minErasureBlocks, maxErasureBlocks)
 	}
 
 	var endpointType EndpointType
