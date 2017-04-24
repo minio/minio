f4202189d30ada4137e545090e859a12ecb123b7
Harshavardhana <harsha@minio.io>
2017-04-24 14:20:23 -0700

    server: Validate path for bad components in a handler.

--- cmd/routers.go
+++ cmd/routers.go
@@ -85,6 +85,8 @@ func configureServerHandler(endpoints EndpointList) (http.Handler, error) {
 
 	// List of some generic handlers which are applied for all incoming requests.
 	var handlerFns = []HandlerFunc{
+		// Validate all the incoming paths.
+		setPathValidityHandler,
 		// Network statistics
 		setHTTPStatsHandler,
 		// Limits all requests size to a maximum fixed limit
