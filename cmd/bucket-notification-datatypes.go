minio/minio

commit 28c5a887de28273e0a9605d7d4b77b84912ed876

event: Set contentType as well under NotificationEvent. (#4003)
This is an enhancement change to to cater support all
the data fields present on the object. Currently
we only send a subset of data which object info
provides us.

It also helps us keep a full namespace mirror on
notification targets for efficient query.

cmd/bucket-notification-datatypes.go
@@ -136,11 +136,13 @@ type bucketMeta struct {
 
 // Notification event object metadata.
 type objectMeta struct {
-	Key       string `json:"key"`
-	Size      int64  `json:"size,omitempty"`
-	ETag      string `json:"eTag,omitempty"`
-	VersionID string `json:"versionId,omitempty"`
-	Sequencer string `json:"sequencer"`
+	Key         string            `json:"key"`
+	Size        int64             `json:"size,omitempty"`
+	ETag        string            `json:"eTag,omitempty"`
+	ContentType string            `json:"contentType:omitempty"`
+	UserDefined map[string]string `json:"userDefined:omitempty"`
+	VersionID   string            `json:"versionId,omitempty"`
+	Sequencer   string            `json:"sequencer"`
 }
 
 const (

cmd/event-notifier.go
@@ -156,6 +156,7 @@ func newNotificationEvent(event eventData) NotificationEvent {
 	if event.Type == ObjectRemovedDelete {
 		nEvent.S3.Object = objectMeta{
 			Key:       escapedObj,
+			VersionID: "1",
 			Sequencer: uniqueID,
 		}
 		return nEvent
@@ -163,10 +164,13 @@ func newNotificationEvent(event eventData) NotificationEvent {
 
 	// For all other events we should set ETag and Size.
 	nEvent.S3.Object = objectMeta{
-		Key:       escapedObj,
-		ETag:      event.ObjInfo.MD5Sum,
-		Size:      event.ObjInfo.Size,
-		Sequencer: uniqueID,
+		Key:         escapedObj,
+		ETag:        event.ObjInfo.MD5Sum,
+		Size:        event.ObjInfo.Size,
+		ContentType: event.ObjInfo.ContentType,
+		UserDefined: event.ObjInfo.UserDefined,
+		VersionID:   "1",
+		Sequencer:   uniqueID,
 	}
 
 	// Success.

commit c04e34f9957a75727ed47db7c8ce206f85248510

Fix bug in JSON representation of object properties
Introduced in #4003

cmd/bucket-notification-datatypes.go
@@ -139,8 +139,8 @@ type objectMeta struct {
 	Key         string            `json:"key"`
 	Size        int64             `json:"size,omitempty"`
 	ETag        string            `json:"eTag,omitempty"`
-	ContentType string            `json:"contentType:omitempty"`
-	UserDefined map[string]string `json:"userDefined:omitempty"`
+	ContentType string            `json:"contentType,omitempty"`
+	UserDefined map[string]string `json:"userDefined,omitempty"`
 	VersionID   string            `json:"versionId,omitempty"`
 	Sequencer   string            `json:"sequencer"`
 }