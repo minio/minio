a37a8d16e76311e6d1bfefe25cc025fa0efd1ca7
Remco Verhoef <remco@dutchcoders.io>
2017-05-08 14:40:27 -0700

    Allow bucket creation in different regions, closes #4287 and #4241
    * I needed to remove the region check from PutBucketHandler

--- cmd/gateway-main.go
+++ cmd/gateway-main.go
@@ -95,14 +95,20 @@ const (
 )
 
 // Returns access and secretkey set from environment variables.
-func mustGetGatewayCredsFromEnv() (accessKey, secretKey string) {
+func mustGetGatewayConfigFromEnv() (string, string, string) {
 	// Fetch access keys from environment variables.
-	accessKey = os.Getenv("MINIO_ACCESS_KEY")
-	secretKey = os.Getenv("MINIO_SECRET_KEY")
+	accessKey := os.Getenv("MINIO_ACCESS_KEY")
+	secretKey := os.Getenv("MINIO_SECRET_KEY")
 	if accessKey == "" || secretKey == "" {
 		fatalIf(errors.New("Missing credentials"), "Access and secret keys are mandatory to run Minio gateway server.")
 	}
-	return accessKey, secretKey
+
+	region := globalMinioDefaultRegion
+	if v := os.Getenv("MINIO_REGION"); v != "" {
+		region = v
+	}
+
+	return accessKey, secretKey, region
 }
 
 // Set browser setting from environment variables
@@ -196,14 +202,13 @@ func gatewayMain(ctx *cli.Context) {
 	}
 
 	// Fetch access and secret key from env.
-	accessKey, secretKey := mustGetGatewayCredsFromEnv()
+	accessKey, secretKey, region := mustGetGatewayConfigFromEnv()
 
 	// Fetch browser env setting
 	mustSetBrowserSettingFromEnv()
 
 	// Initialize new gateway config.
-
-	newGatewayConfig(accessKey, secretKey, globalMinioDefaultRegion)
+	newGatewayConfig(accessKey, secretKey, region)
 
 	// Get quiet flag from command line argument.
 	quietFlag := ctx.Bool("quiet") || ctx.GlobalBool("quiet")
