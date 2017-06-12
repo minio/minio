1c89b48cd6028a7f7e2cbbbd5b00143ed2c29dfc
frankw <fwessels@xs4all.nl>
2017-06-12 13:51:09 -0700

    Use UTCNow() instead of time.Now().UTC()

--- cmd/server-mux_test.go
+++ cmd/server-mux_test.go
@@ -390,7 +390,7 @@ func TestServerListenAndServeTLS(t *testing.T) {
 			Transport: tr,
 		}
 		// Keep trying the server until it's accepting connections
-		start := time.Now().UTC()
+		start := UTCNow()
 		for {
 			res, _ := client.Get("https://" + addr)
 			if res != nil && res.StatusCode == http.StatusOK {
@@ -398,7 +398,7 @@ func TestServerListenAndServeTLS(t *testing.T) {
 			}
 			// Explicit check to terminate loop after 5 minutes
 			// (for investigational purpose of issue #4461)
-			if time.Now().UTC().Sub(start) >= 5*time.Minute {
+			if UTCNow().Sub(start) >= 5*time.Minute {
 				t.Fatalf("Failed to establish connection after 5 minutes")
 			}
 		}
