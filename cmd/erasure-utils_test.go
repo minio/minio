d8cf472c6d3782d2628c732b716503637bf84790
Andreas Auernhammer <aead@mail.de>
2017-06-22 19:46:35 -0700

    erasure: add additional test to detect invalid algorithms
    Add a test to ensure that currently only bitrot algorithms without keys
    can be used.

--- cmd/erasure-utils_test.go
+++ cmd/erasure-utils_test.go
@@ -18,9 +18,12 @@ package cmd
 
 import (
 	"bytes"
+	"crypto/rand"
 	"errors"
 	"io"
 	"testing"
+
+	"github.com/minio/minio/pkg/bitrot"
 )
 
 // Test validates the number hash writers returned.
@@ -154,6 +157,24 @@ func TestCopyBuffer(t *testing.T) {
 		if actualErr == nil && testCase.shouldPass {
 			if !bytes.Equal(testCase.expectedResult, buffers[2].Bytes()) {
 				t.Errorf("Test %d: copied buffer differs from the expected one.", i+1)
+			}
+		}
+	}
+}
+
+func TestAvailableBitrotAlgorithms(t *testing.T) {
+	algorithms := []bitrot.Algorithm{bitrot.SHA256, bitrot.BLAKE2b512, bitrot.Poly1305, bitrot.GHASH}
+	for _, alg := range algorithms {
+		if alg.Available() {
+			key, err := alg.GenerateKey(rand.Reader)
+			if err != nil {
+				t.Errorf("Algorithm %s: Failed to generate key", alg.String())
+			}
+			if len(key) != 0 {
+				t.Errorf("Algorithm %s: Generated key length is #%d instead of 0", alg.String(), len(key))
+			}
+			if _, err := alg.New(nil); err != nil {
+				t.Errorf("Algorithm %s: failed to create instance with nil key", alg.String())
 			}
 		}
 	}
