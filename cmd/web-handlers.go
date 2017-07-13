63e8c535c8ab6046e64851f54393e13699fea3aa
Brendan Ashworth <brendan.ashworth@me.com>
2017-07-12 19:16:17 -0700

    browser: add ability to delete bucket
    This commit adds the ability to delete empty buckets to the brower. It
    does this with a new RPC call, DeleteBucket. This requires the proper
    authentication and adds tests. It only deletes a bucket that is empty =
    no objects.
    
    This also integrates it into the UI with a dropdown for each bucket.
    
    Fixes: https://github.com/minio/minio/issues/4166

--- cmd/web-handlers.go
+++ cmd/web-handlers.go
@@ -143,6 +143,34 @@ func (web *webAPIHandlers) MakeBucket(r *http.Request, args *MakeBucketArgs, rep
 	return nil
 }
 
+// RemoveBucketArgs - remove bucket args.
+type RemoveBucketArgs struct {
+	BucketName string `json:"bucketName"`
+}
+
+// DeleteBucket - removes a bucket, must be empty.
+func (web *webAPIHandlers) DeleteBucket(r *http.Request, args *RemoveBucketArgs, reply *WebGenericRep) error {
+	objectAPI := web.ObjectAPI()
+	if objectAPI == nil {
+		return toJSONError(errServerNotInitialized)
+	}
+	if !isHTTPRequestValid(r) {
+		return toJSONError(errAuthentication)
+	}
+
+	bucketLock := globalNSMutex.NewNSLock(args.BucketName, "")
+	bucketLock.Lock()
+	defer bucketLock.Unlock()
+
+	err := objectAPI.DeleteBucket(args.BucketName)
+	if err != nil {
+		return toJSONError(err, args.BucketName)
+	}
+
+	reply.UIVersion = browser.UIVersion
+	return nil
+}
+
 // ListBucketsRep - list buckets response
 type ListBucketsRep struct {
 	Buckets   []WebBucketInfo `json:"buckets"`
