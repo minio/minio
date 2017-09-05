89beffec5493bb583b17f919da6c1e689a48518f
Harshavardhana <harsha@minio.io>
2017-09-05 03:00:10 -0700

    Increase default read/write timeouts from 30sec to 15minutes
    The default timeout of 30secs is not enough for high latency
    environments, change these values to use 15 minutes instead.
    
    With 30secs I/O timeouts seem to be quite common, this leads
    to pretty much most SDKs and clients reconnect. This in-turn
    causes significant performance problems. On a low latency
    interconnect this can be quite challenging to transfer large
    amounts of data. Setting this value to 15minutes covers
    pretty much all known cases.
    
    This PR was tested with `wondershaper <NIC> 20000 20000` by
    limiting the network bandwidth to 20Mbit/sec. Default timeout
    caused a significant amount of I/O timeouts, leading to
    constant retires from the client. This seems to be more common
    with tools like rclone, restic which have high concurrency set
    by default. Once the value was fixed to 15minutes i/o timeouts
    stopped and client could steadily upload data to the server
    even while saturating the network.
    
    Fixes #4670

--- cmd/server-main.go
+++ cmd/server-main.go
@@ -193,6 +193,8 @@ func serverMain(ctx *cli.Context) {
 	initGlobalAdminPeers(globalEndpoints)
 
 	globalHTTPServer = miniohttp.NewServer([]string{globalMinioAddr}, handler, globalTLSCertificate)
+	globalHTTPServer.ReadTimeout = globalConnReadTimeout
+	globalHTTPServer.WriteTimeout = globalConnWriteTimeout
 	globalHTTPServer.UpdateBytesReadFunc = globalConnStats.incInputBytes
 	globalHTTPServer.UpdateBytesWrittenFunc = globalConnStats.incOutputBytes
 	globalHTTPServer.ErrorLogFunc = errorIf
