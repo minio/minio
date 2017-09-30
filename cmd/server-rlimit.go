96282472c5fdde34d667b4f56cd735378fda633d
Anis Elleuch <anis@minio.io>
2017-09-30 20:22:30 +0100

    Push max threads to little less than kernel limit
    Let Minio server use more threads than allowed by golang runtime. This
    is important to better deal with high load.

--- cmd/server-rlimit.go
+++ cmd/server-rlimit.go
@@ -16,9 +16,24 @@
 
 package cmd
 
-import "github.com/minio/minio/pkg/sys"
+import (
+	"runtime/debug"
+
+	"github.com/minio/minio/pkg/sys"
+)
 
 func setMaxResources() (err error) {
+	// Set the Go runtime max threads threshold to 90% of kernel setting.
+	// Do not return when an error when encountered since it is not a crucial task.
+	sysMaxThreads, mErr := sys.GetMaxThreads()
+	if mErr == nil {
+		minioMaxThreads := (sysMaxThreads * 90) / 100
+		// Only set max threads if it is greater than the default one
+		if minioMaxThreads > 10000 {
+			debug.SetMaxThreads(minioMaxThreads)
+		}
+	}
+
 	var maxLimit uint64
 
 	// Set open files limit to maximum.
