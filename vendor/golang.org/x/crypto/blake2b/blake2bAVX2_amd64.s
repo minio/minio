9212ac749f8c8555f0290e18027027b3a0a46990
Andreas Auernhammer <aead@mail.de>
2018-04-30 20:28:20 +0200

    vendor x/sys/cpu and update x/crypto/blake2b
    This change updates the vendor'd x/crypto/blake2b package and
    adds x/sys/cpu.
    
    This change fixes an issue for Go1.11 affecting x/crypto/blake2b
    since G1.11 removes the runtime-internal functions `suports_avx()` and
    `supports_avx2()`.

--- vendor/golang.org/x/crypto/blake2b/blake2bAVX2_amd64.s
+++ vendor/golang.org/x/crypto/blake2b/blake2bAVX2_amd64.s
@@ -748,15 +748,3 @@ noinc:
 
 	MOVQ BP, SP
 	RET
-
-// func supportsAVX2() bool
-TEXT ·supportsAVX2(SB), 4, $0-1
-	MOVQ runtime·support_avx2(SB), AX
-	MOVB AX, ret+0(FP)
-	RET
-
-// func supportsAVX() bool
-TEXT ·supportsAVX(SB), 4, $0-1
-	MOVQ runtime·support_avx(SB), AX
-	MOVB AX, ret+0(FP)
-	RET
