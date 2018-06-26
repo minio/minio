1bd56302d6b62c8c3431cbce75345b1617ff8fbb
Harshavardhana <harsha@minio.io>
2018-06-26 16:23:14 -0700

    disable disk-usage when export is root mount path
    disk usage crawling is not needed when a tenant
    is not sharing the same disk for multiple other
    tenants. This PR adds an optimization when we
    see a setup uses entire disk, we simply rely on
    statvfs() to give us total usage.
    
    This PR also additionally adds low priority
    scheduling for usage check routine, such that
    other go-routines blocked will be automatically
    unblocked and prioritized before usage.

--- pkg/mountinfo/mountinfo_windows.go
+++ pkg/mountinfo/mountinfo_windows.go
@@ -1,7 +1,7 @@
-// +build !linux,!windows
+// +build windows
 
 /*
- * Minio Cloud Storage, (C) 2017, 2018 Minio, Inc.
+ * Minio Cloud Storage, (C) 2018 Minio, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,13 +18,42 @@
 
 package mountinfo
 
+import (
+	"os"
+)
+
 // CheckCrossDevice - check if any input path has multiple sub-mounts.
 // this is a dummy function and returns nil for now.
 func CheckCrossDevice(paths []string) error {
 	return nil
 }
 
+// fileExists checks if specified file exists.
+func fileExists(filename string) (bool, error) {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
 // IsLikelyMountPoint determines if a directory is a mountpoint.
 func IsLikelyMountPoint(file string) bool {
+	stat, err := os.Lstat(file)
+	if err != nil {
+		return false
+	}
+
+	// If current file is a symlink, then it is a mountpoint.
+	if stat.Mode()&os.ModeSymlink != 0 {
+		target, err := os.Readlink(file)
+		if err != nil {
+			return false
+		}
+		exists, _ := fileExists(target)
+		return exists
+	}
+
 	return false
 }
