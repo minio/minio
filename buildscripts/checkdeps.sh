317e648c0dbbdcbb6669c7280cef084a54683ab3
Rafael Peria de Sene <rpsene@linux.vnet.ibm.com>
2018-07-03 13:42:19 -0300

    Add support for ppc64le (#6116)
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- buildscripts/checkdeps.sh
+++ buildscripts/checkdeps.sh
@@ -89,11 +89,11 @@ check_minimum_version() {
 
 assert_is_supported_arch() {
     case "${ARCH}" in
-        x86_64 | amd64 | aarch64 | arm* )
+        x86_64 | amd64 | aarch64 | ppc64le | arm* )
             return
             ;;
         *)
-            echo "Arch '${ARCH}' is not supported. Supported Arch: [x86_64, amd64, aarch64, arm*]"
+            echo "Arch '${ARCH}' is not supported. Supported Arch: [x86_64, amd64, aarch64, ppc64le, arm*]"
             exit 1
     esac
 }
