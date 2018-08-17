09c0a977f57c6296589b2ede3e339ecb13be4eab
Harshavardhana <harsha@minio.io>
2018-08-17 12:21:49 -0700

    Add select docs and fix return values for Select API

---
+++ docs/select/select.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env/env python3
+import boto3
+
+s3 = boto3.client('s3',
+                  endpoint_url='http://localhost:9000',
+                  aws_access_key_id='minio',
+                  aws_secret_access_key='minio123',
+                  region_name='us-east-1')
+
+r = s3.select_object_content(
+    Bucket='mycsvbucket',
+    Key='sampledata/TotalPopulation.csv.gz',
+    ExpressionType='SQL',
+    Expression="select * from s3object s where s.Location like '%United States%'",
+    InputSerialization={
+        'CSV': {
+            "FileHeaderInfo": "USE",
+        },
+        'CompressionType': 'GZIP',
+    },
+    OutputSerialization={'CSV': {}},
+)
+
+for event in r['Payload']:
+    if 'Records' in event:
+        records = event['Records']['Payload'].decode('utf-8')
+        print(records)
+    elif 'Stats' in event:
+        statsDetails = event['Stats']['Details']
+        print("Stats details bytesScanned: ")
+        print(statsDetails['BytesScanned'])
+        print("Stats details bytesProcessed: ")
+        print(statsDetails['BytesProcessed'])
