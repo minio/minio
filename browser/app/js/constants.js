5e7ccc983d87d0378dd967a99f1620cb4d40e2c8
Kaan Kabalak <kaankabalak@gmail.com>
2018-08-30 16:20:27 -0600

    UI: Handle policies listed in mc as 'none' by not showing them (#6353)

--- browser/app/js/constants.js
+++ browser/app/js/constants.js
@@ -23,6 +23,7 @@ export const minioBrowserPrefix = p.slice(0, p.indexOf("/", 1))
 export const READ_ONLY = "readonly"
 export const WRITE_ONLY = "writeonly"
 export const READ_WRITE = "readwrite"
+export const NONE = "none"
 
 export const SHARE_OBJECT_EXPIRY_DAYS = 5
 export const SHARE_OBJECT_EXPIRY_HOURS = 0
