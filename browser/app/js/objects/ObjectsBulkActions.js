faac79eb8dbbff816dbcc2b8f745625631236226
rushenn <rushenn123@gmail.com>
2018-09-27 22:09:49 +0530

    Overall UI adjustments and enhancements

--- browser/app/js/objects/ObjectsBulkActions.js
+++ browser/app/js/objects/ObjectsBulkActions.js
@@ -89,6 +89,29 @@ export class ObjectsBulkActions extends React.Component {
     } = this.props
     const loggedIn = web.LoggedIn()
 
+    const icons = {
+      share: (
+        <svg width="13" height="14">
+          <path d="M10.338 9.31c-.507 0-.96.196-1.307.503l-4.753-2.71c.033-.15.06-.3.06-.457s-.027-.307-.06-.457l4.7-2.682c.36.326.833.53 1.36.53 1.107 0 2-.874 2-1.958s-.893-1.958-2-1.958-2 .874-2 1.958a2.11 2.11 0 0 0 .06.457l-4.7 2.682c-.36-.326-.833-.53-1.36-.53-1.107 0-2 .874-2 1.958s.893 1.958 2 1.958a2.01 2.01 0 0 0 1.36-.529L8.445 10.8c-.033.137-.053.28-.053.424-.001 1.05.873 1.906 1.946 1.906s1.947-.855 1.947-1.906-.874-1.905-1.947-1.905z" />
+        </svg>
+      ),
+      download: (
+        <svg width="12" height="14">
+          <path d="M11.338 5.088H8.195V.5H3.48v4.588H.338l5.5 5.353 5.5-5.353zm-11 6.883v1.53h11v-1.53h-11z" />
+        </svg>
+      ),
+      trash: (
+        <svg width="11" height="14">
+          <path d="M1.214 12.022c0 .813.643 1.478 1.43 1.478h5.714c.786 0 1.43-.665 1.43-1.478v-8.87H1.214v8.87zM10.5 1.163H8L7.286.5H3.714L3 1.163H.5V2.5h10V1.163z" />
+        </svg>
+      ),
+      close: (
+        <svg width="12" height="12">
+          <path d="M11.316 1.948L10.318.95l-3.96 3.96L2.398.95l-.998.998 3.96 3.96-3.96 3.96 1 1 3.96-3.96 3.96 3.96 1-1-3.96-3.96z" />
+        </svg>
+      )
+    }
+
     return (
       <React.Fragment>
         <div
@@ -107,40 +130,39 @@ export class ObjectsBulkActions extends React.Component {
             }
             disabled={!checkedObjectsCount}
           >
-            <img src={iconDelete} alt="" />
+            {icons.trash}
             <span>Delete selected</span>
           </button>
-
-          {loggedIn && (
-            <button
-              id="share-checked"
-              className="object-actions__item"
-              onClick={this.shareObject.bind(this)}
-              disabled={checkedObjectsCount != 1}
-            >
-              <img src={iconShare} alt="" />
-              <span>Share</span>
-            </button>
-          )}
           <button
             id="download-checked"
             className="object-actions__item"
             onClick={this.handleDownload.bind(this)}
             disabled={!checkedObjectsCount}
           >
-            <img src={iconDownload} alt="" />
+            {icons.download}
             <span>
               Download {checkedObjects.length > 1 ? " all as zip" : " object"}
             </span>
           </button>
-          <span
+          {loggedIn &&
+            checkedObjectsCount == 1 && (
+              <button
+                id="share-checked"
+                className="object-actions__item"
+                onClick={this.shareObject.bind(this)}
+                disabled={checkedObjectsCount != 1}
+              >
+                {icons.share}
+                <span>Share</span>
+              </button>
+            )}
+          <button
             id="close-bulk-actions"
             className="object-actions__item object-actions__close"
             onClick={clearChecked}
           >
-            <img src={iconClose} alt="" />
-            <span>Cancel</span>
-          </span>
+            {icons.close}
+          </button>
         </div>
 
         {showShareObjectModal && <ShareObjectModal object={object} />}
