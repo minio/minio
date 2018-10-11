774debbf4b6887e098177990a8c5f09234da1f69
Ersan <ersan.bozduman@gmail.com>
2018-10-11 00:21:22 -0700

    WIP: New flat configuration file

---
+++ pkg/configuration/examples/set-help-example.go
@@ -0,0 +1,39 @@
+/*
+ * Minio Cloud Storage, (C) 2018 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/minio/minio/cmd/logger"
+	"github.com/minio/minio/pkg/configuration"
+)
+
+func main() {
+	var err error
+
+	serverConfig := &configuration.ServerConfig{}
+	// Load configuration data from disk to memory
+	if err = serverConfig.Load(); err != nil {
+		logger.FatalIf(err, "Failed to load configuration data: ")
+	}
+
+	kv, err := serverConfig.GetHandler()
+	fmt.Printf("GetHandler > %+v\n\n", kv)
+
+}
