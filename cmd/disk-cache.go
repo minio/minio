ec64bc36fc7f8c6b9b1f17828291cdee8e611b31
Pontus Leitzler <leitzler@gmail.com>
2018-10-15 15:10:50 +0200

    Add missing error check in GetObjectNInfo

--- cmd/disk-cache.go
+++ cmd/disk-cache.go
@@ -186,6 +186,9 @@ func (c cacheObjects) getMetadata(objInfo ObjectInfo) map[string]string {
 func (c cacheObjects) GetObjectNInfo(ctx context.Context, bucket, object string, rs *HTTPRangeSpec, h http.Header, lockType LockType, opts ObjectOptions) (gr *GetObjectReader, err error) {
 
 	objInfo, bkErr := c.GetObjectInfoFn(ctx, bucket, object, opts)
+	if bkErr != nil {
+		return nil, bkErr
+	}
 
 	if c.isCacheExclude(bucket, object) || !objInfo.IsCacheable() {
 		return c.GetObjectNInfoFn(ctx, bucket, object, rs, h, writeLock, opts)
