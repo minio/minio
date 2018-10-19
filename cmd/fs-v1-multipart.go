8bbfb8a1724d0af7a5d9c16c6087ca79b9233daa
Aarushi Arya <aarushiarya25@gmail.com>
2018-10-19 10:12:46 -0700

    Remove files in tmp

--- cmd/fs-v1-multipart.go
+++ cmd/fs-v1-multipart.go
@@ -676,6 +676,11 @@ func (fs *FSObjects) AbortMultipartUpload(ctx context.Context, bucket, object, u
 	}
 
 	fs.appendFileMapMu.Lock()
+	// Remove file in tmp folder
+	file := fs.appendFileMap[uploadID]
+	if file != nil {
+		fsRemoveFile(ctx, file.filePath)
+	}
 	delete(fs.appendFileMap, uploadID)
 	fs.appendFileMapMu.Unlock()
 
@@ -689,17 +694,12 @@ func (fs *FSObjects) AbortMultipartUpload(ctx context.Context, bucket, object, u
 		return toObjectErr(err, bucket, object)
 	}
 
-	// Remove the folder created in tmp
-	tmpFolderPath := pathJoin(fs.fsPath, minioMetaTmpBucket, fs.fsUUID)
-	fsRemoveAll(ctx, tmpFolderPath)
-
 	// Ignore the error returned as Windows fails to remove directory if a file in it
 	// is Open()ed by the backgroundAppend()
 	fsRemoveAll(ctx, uploadIDDir)
 
 	// Remove the folder created in multipart
-	multipartFolderPath := pathJoin(fs.fsPath, minioMetaMultipartBucket, getSHA256Hash([]byte(pathJoin(bucket, object))))
-	fsRemoveAll(ctx, multipartFolderPath)
+	fsRemoveDir(ctx, fs.getUploadIDDir(bucket, object, ""))
 
 	return nil
 }
