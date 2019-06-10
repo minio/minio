minio/minio

commit 502456db911ce2b87c2de3dd09b37b83f51ba6e1

Accept simpler response from OPA (#7637)

pkg/iam/policy/opa.go
@@ -123,14 +123,12 @@ func (o *Opa) IsAllowed(args Args) bool {
 
 	// Handle OPA response
 	type opaResponse struct {
-		Result struct {
-			Allow bool `json:"allow"`
-		} `json:"result"`
+		Allow bool `json:"allow"`
 	}
 	var result opaResponse
 	if err = json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		return false
 	}
 
-	return result.Result.Allow
+	return result.Allow
 }

commit a28d5582c088895da2227cad39a516548c4712e4

Fix OPA result response handling
Also update the document with updated rego policy
and updated OPA agent REST API.

This PR is to fix a regression caused by PR #7637

docs/sts/docker-compose.yml
@@ -1,7 +1,7 @@
 version: '2'
 services:
   opa:
-    image: openpolicyagent/opa:0.9.1
+    image: openpolicyagent/opa:0.11.0
     ports:
       - 8181:8181
     command:

docs/sts/putobject.rego
@@ -2,7 +2,9 @@ package httpapi.authz
 
 import input as http_api
 
-allow {
- input.action = "s3:PutObject"
- input.owner = false
+default allow = false
+
+allow = true {
+ http_api.action = "s3:PutObject"
+ http_api.owner = false
 }

pkg/iam/policy/opa.go
@@ -20,6 +20,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -121,14 +122,36 @@ func (o *Opa) IsAllowed(args Args) bool {
 	}
 	defer o.args.CloseRespFn(resp.Body)
 
-	// Handle OPA response
-	type opaResponse struct {
-		Allow bool `json:"allow"`
-	}
-	var result opaResponse
-	if err = json.NewDecoder(resp.Body).Decode(&result); err != nil {
+	// Read the body to be saved later.
+	opaRespBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
 		return false
 	}
 
-	return result.Allow
+	// Handle large OPA responses when OPA URL is of
+	// form http://localhost:8181/v1/data/httpapi/authz
+	type opaResultAllow struct {
+		Result struct {
+			Allow bool `json:"allow"`
+		} `json:"result"`
+	}
+
+	// Handle simpler OPA responses when OPA URL is of
+	// form http://localhost:8181/v1/data/httpapi/authz/allow
+	type opaResult struct {
+		Result bool `json:"result"`
+	}
+
+	respBody := bytes.NewReader(opaRespBytes)
+
+	var result opaResult
+	if err = json.NewDecoder(respBody).Decode(&result); err != nil {
+		respBody.Seek(0, 0)
+		var resultAllow opaResultAllow
+		if err = json.NewDecoder(respBody).Decode(&resultAllow); err != nil {
+			return false
+		}
+		return resultAllow.Result.Allow
+	}
+	return result.Result
 }