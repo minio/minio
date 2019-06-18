minio/minio

commit 510ec153b96c52c67cb9ae816b3c2158e89c04a3

Refreshing notification system should not erase the rules-map of other buckets (#7758)
Fixes #7707

cmd/notification.go
@@ -656,7 +656,9 @@ func (sys *NotificationSys) refresh(objAPI ObjectLayer) error {
 		ctx := logger.SetReqInfo(context.Background(), &logger.ReqInfo{BucketName: bucket.Name})
 		config, err := readNotificationConfig(ctx, objAPI, bucket.Name)
 		if err != nil && err != errNoSuchNotifications {
-			return err
+			if _, ok := err.(*event.ErrARNNotFound); ok {
+				continue
+			}
 		}
 		if err == errNoSuchNotifications {
 			continue

commit b0b92f65646ee805cf90846d790d94df8f05ec22

[notify] Make sure to return when quorum is missing
Fixes a regression introduced in 510ec153b96c52c67cb9ae816b3c2158e89c04a3

cmd/notification.go
@@ -659,6 +659,7 @@ func (sys *NotificationSys) refresh(objAPI ObjectLayer) error {
 			if _, ok := err.(*event.ErrARNNotFound); ok {
 				continue
 			}
+			return err
 		}
 		if err == errNoSuchNotifications {
 			continue