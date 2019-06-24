03e5558530a19ee49f6662070c59271137392d39
Krishna Srinivas <krishna.srinivas@gmail.com>
2019-06-24 00:57:32 -0700

    Fail putObject in case client prematurely disconnects

--- cmd/object-handlers.go
+++ cmd/object-handlers.go
@@ -1064,6 +1064,8 @@ func (api objectAPIHandlers) PutObjectHandler(w http.ResponseWriter, r *http.Req
 	bucket := vars["bucket"]
 	object := vars["object"]
 
+	r.Body = &detectDisconnect{r.Body, r.Context().Done()}
+
 	// X-Amz-Copy-Source shouldn't be set for this call.
 	if _, ok := r.Header["X-Amz-Copy-Source"]; ok {
 		writeErrorResponse(ctx, w, errorCodes.ToAPIErr(ErrInvalidCopySource), r.URL, guessIsBrowserReq(r))
