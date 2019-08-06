92b285d2e6faed5d76883e99fe76134b0d1de5c3
Jakob Ackermann <das7pad@outlook.com>
2019-08-06 11:18:53 +0200

    [generic-handlers] redirect all favicon variants
    REF: 22b4fe0a51bc58a63a11b3d5311b17bb5909f70c

--- cmd/generic-handlers.go
+++ cmd/generic-handlers.go
@@ -178,7 +178,9 @@ func getRedirectLocation(urlPath string) (rLocation string) {
 		"/",
 		"/webrpc",
 		"/login",
-		"/favicon.ico",
+		"/favicon-16x16.png",
+		"/favicon-32x32.png",
+		"/favicon-96x96.png",
 	}, urlPath) {
 		rLocation = minioReservedBucketPath + urlPath
 	}
