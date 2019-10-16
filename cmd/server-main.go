e38731c59b8c0ffc8b25d8e2d9fd3e0d03215c1e
Poorna Krishnamoorthy <poorna@minio.io>
2019-10-15 18:26:22 -0700

    Remove cache env from server help message

--- cmd/server-main.go
+++ cmd/server-main.go
@@ -82,12 +82,6 @@ ENVIRONMENT VARIABLES:
   BROWSER:
      MINIO_BROWSER: To disable web browser access, set this value to "off".
 
-  CACHE:
-     MINIO_CACHE_DRIVES: List of mounted drives or directories delimited by ";".
-     MINIO_CACHE_EXCLUDE: List of cache exclusion patterns delimited by ";".
-     MINIO_CACHE_EXPIRY: Cache expiry duration in days.
-     MINIO_CACHE_MAXUSE: Maximum permitted usage of the cache in percentage (0-100).
-
   DOMAIN:
      MINIO_DOMAIN: To enable virtual-host-style requests, set this value to MinIO host domain name.
 
