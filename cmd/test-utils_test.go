8778bd39155c4caf63e5bdbbb14a6d005a60b503
Harshavardhana <harsha@minio.io>
2020-01-17 14:13:57 -0800

    log formatting only the first time

--- cmd/test-utils_test.go
+++ cmd/test-utils_test.go
@@ -193,7 +193,7 @@ func prepareXLSets32() (ObjectLayer, []string, error) {
 
 	endpoints := append(endpoints1, endpoints2...)
 	fsDirs := append(fsDirs1, fsDirs2...)
-	format, err := waitForFormatXL(true, endpoints, 2, 16, "")
+	format, err := waitForFormatXL(true, endpoints, 1, 2, 16, "")
 	if err != nil {
 		removeRoots(fsDirs)
 		return nil, nil, err
