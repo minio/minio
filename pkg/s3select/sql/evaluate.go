56d14ea861226520ce3222e8f82a73c2b3434d26
Klaus Post <klauspost@gmail.com>
2020-04-24 11:15:35 +0200

    select: Infer types for comparison
    Infer types for IN comparisons.

--- pkg/s3select/sql/evaluate.go
+++ pkg/s3select/sql/evaluate.go
@@ -250,6 +250,9 @@ func (e *In) evalInNode(r Record, lhs *Value) (*Value, error) {
 	// Compare two values in terms of in-ness.
 	var cmp func(a, b Value) bool
 	cmp = func(a, b Value) bool {
+		// Convert if needed.
+		inferTypesForCmp(&a, &b)
+
 		if a.Equals(b) {
 			return true
 		}
