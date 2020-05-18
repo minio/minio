a5349b8e482d2ba9894ff07bcdb00051617145ab
Klaus Post <klauspost@gmail.com>
2020-05-18 17:12:24 +0200

    Set Deployment ID before starting handlers
    Global handler ID is added to response headers, so initialize it before the server starts.
    
    Fixes #9634

--- cmd/gateway-main.go
+++ cmd/gateway-main.go
@@ -96,6 +96,10 @@ func ValidateGatewayArguments(serverAddr, endpointAddr string) error {
 
 // StartGateway - handler for 'minio gateway <name>'.
 func StartGateway(ctx *cli.Context, gw Gateway) {
+	// This is only to uniquely identify each gateway deployments.
+	globalDeploymentID = env.Get("MINIO_GATEWAY_DEPLOYMENT_ID", mustGetUUID())
+	logger.SetDeploymentID(globalDeploymentID)
+
 	if gw == nil {
 		logger.FatalIf(errUnexpected, "Gateway implementation not initialized")
 	}
@@ -260,9 +264,6 @@ func StartGateway(ctx *cli.Context, gw Gateway) {
 		// is only enabled for "NAS" gateway.
 		globalConfigSys.WatchConfigNASDisk(GlobalContext, newObject)
 	}
-	// This is only to uniquely identify each gateway deployments.
-	globalDeploymentID = env.Get("MINIO_GATEWAY_DEPLOYMENT_ID", mustGetUUID())
-	logger.SetDeploymentID(globalDeploymentID)
 
 	if globalEtcdClient != nil {
 		// ****  WARNING ****
