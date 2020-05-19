9baeda781a60c88875042a15502fb026a57d375b
Anis Elleuch <vadmeste@users.noreply.github.com>
2020-05-19 22:27:20 +0100

    fix storage info output with unordered endpoints arguments (#9610)
    Shuffling arguments that we pass to MinIO server are supported. However,
    when that happens, Prometheus returns wrong information about disks usage
    and online/offline status.
    
    The commit fixes the issue by avoiding relying on xl.endpoints since
    it is not ordered.

--- cmd/object-api-common.go
+++ cmd/object-api-common.go
@@ -82,7 +82,7 @@ func dirObjectInfo(bucket, object string, size int64, metadata map[string]string
 // Depending on the disk type network or local, initialize storage API.
 func newStorageAPI(endpoint Endpoint) (storage StorageAPI, err error) {
 	if endpoint.IsLocal {
-		storage, err := newPosix(endpoint.Path)
+		storage, err := newPosix(endpoint.Path, endpoint.Host)
 		if err != nil {
 			return nil, err
 		}
