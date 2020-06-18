minio/minio

commit f5e1b3d09e3ab371421d7e01fa8b31942d9611ee

fix: initialize config once per startup (#9851)

cmd/config-current.go
@@ -311,8 +311,6 @@ func validateConfig(s config.Config) error {
 		globalNotificationSys.ConfiguredTargetIDs())
 }
 
-var syncEtcdOnce sync.Once
-
 func lookupConfigs(s config.Config) {
 	ctx := GlobalContext
 
@@ -335,16 +333,30 @@ func lookupConfigs(s config.Config) {
 	}
 
 	if etcdCfg.Enabled {
-		syncEtcdOnce.Do(func() {
-			globalEtcdClient, err = etcd.New(etcdCfg)
+		globalEtcdClient, err = etcd.New(etcdCfg)
+		if err != nil {
+			if globalIsGateway {
+				logger.FatalIf(err, "Unable to initialize etcd config")
+			} else {
+				logger.LogIf(ctx, fmt.Errorf("Unable to initialize etcd config: %w", err))
+			}
+		}
+		if len(globalDomainNames) != 0 && !globalDomainIPs.IsEmpty() && globalEtcdClient != nil {
+			globalDNSConfig, err = dns.NewCoreDNS(etcdCfg.Config,
+				dns.DomainNames(globalDomainNames),
+				dns.DomainIPs(globalDomainIPs),
+				dns.DomainPort(globalMinioPort),
+				dns.CoreDNSPath(etcdCfg.CoreDNSPath),
+			)
 			if err != nil {
 				if globalIsGateway {
-					logger.FatalIf(err, "Unable to initialize etcd config")
+					logger.FatalIf(err, "Unable to initialize DNS config")
 				} else {
-					logger.LogIf(ctx, fmt.Errorf("Unable to initialize etcd config: %w", err))
+					logger.LogIf(ctx, fmt.Errorf("Unable to initialize DNS config for %s: %w",
+						globalDomainNames, err))
 				}
 			}
-		})
+		}
 	}
 
 	// Bucket federation is 'true' only when IAM assets are not namespaced
@@ -353,19 +365,6 @@ func lookupConfigs(s config.Config) {
 	// we assume that users are interested in global bucket support
 	// but not federation.
 	globalBucketFederation = etcdCfg.PathPrefix == "" && etcdCfg.Enabled
-
-	if len(globalDomainNames) != 0 && !globalDomainIPs.IsEmpty() && globalEtcdClient != nil {
-		globalDNSConfig, err = dns.NewCoreDNS(etcdCfg.Config,
-			dns.DomainNames(globalDomainNames),
-			dns.DomainIPs(globalDomainIPs),
-			dns.DomainPort(globalMinioPort),
-			dns.CoreDNSPath(etcdCfg.CoreDNSPath),
-		)
-		if err != nil {
-			logger.LogIf(ctx, fmt.Errorf("Unable to initialize DNS config for %s: %w",
-				globalDomainNames, err))
-		}
-	}
 
 	globalServerRegion, err = config.LookupRegion(s[config.RegionSubSys][config.Default])
 	if err != nil {
@@ -571,6 +570,8 @@ func newServerConfig() config.Config {
 	return config.New()
 }
 
+var lookupConfigOnce sync.Once
+
 // newSrvConfig - initialize a new server config, saves env parameters if
 // found, otherwise use default parameters
 func newSrvConfig(objAPI ObjectLayer) error {
@@ -578,7 +579,9 @@ func newSrvConfig(objAPI ObjectLayer) error {
 	srvCfg := newServerConfig()
 
 	// Override any values from ENVs.
-	lookupConfigs(srvCfg)
+	lookupConfigOnce.Do(func() {
+		lookupConfigs(srvCfg)
+	})
 
 	// hold the mutex lock before a new config is assigned.
 	globalServerConfigMu.Lock()
@@ -602,7 +605,9 @@ func loadConfig(objAPI ObjectLayer) error {
 	}
 
 	// Override any values from ENVs.
-	lookupConfigs(srvCfg)
+	lookupConfigOnce.Do(func() {
+		lookupConfigs(srvCfg)
+	})
 
 	// hold the mutex lock before a new config is assigned.
 	globalServerConfigMu.Lock()

cmd/gateway-main.go
@@ -162,7 +162,9 @@ func StartGateway(ctx *cli.Context, gw Gateway) {
 	srvCfg := newServerConfig()
 
 	// Override any values from ENVs.
-	lookupConfigs(srvCfg)
+	lookupConfigOnce.Do(func() {
+		lookupConfigs(srvCfg)
+	})
 
 	// hold the mutex lock before a new config is assigned.
 	globalServerConfigMu.Lock()

commit 34da7339c82bc64850ef35de0ba65214d1126c22

allow loading some from config and some values from ENVs
A regression perhaps introduced in #9851

cmd/config-current.go
@@ -333,15 +333,18 @@ func lookupConfigs(s config.Config) {
 	}
 
 	if etcdCfg.Enabled {
-		globalEtcdClient, err = etcd.New(etcdCfg)
-		if err != nil {
-			if globalIsGateway {
-				logger.FatalIf(err, "Unable to initialize etcd config")
-			} else {
-				logger.LogIf(ctx, fmt.Errorf("Unable to initialize etcd config: %w", err))
+		if globalEtcdClient == nil {
+			globalEtcdClient, err = etcd.New(etcdCfg)
+			if err != nil {
+				if globalIsGateway {
+					logger.FatalIf(err, "Unable to initialize etcd config")
+				} else {
+					logger.LogIf(ctx, fmt.Errorf("Unable to initialize etcd config: %w", err))
+				}
 			}
 		}
-		if len(globalDomainNames) != 0 && !globalDomainIPs.IsEmpty() && globalEtcdClient != nil {
+
+		if len(globalDomainNames) != 0 && !globalDomainIPs.IsEmpty() && globalEtcdClient != nil && globalDNSConfig == nil {
 			globalDNSConfig, err = dns.NewCoreDNS(etcdCfg.Config,
 				dns.DomainNames(globalDomainNames),
 				dns.DomainIPs(globalDomainIPs),
@@ -570,8 +573,6 @@ func newServerConfig() config.Config {
 	return config.New()
 }
 
-var lookupConfigOnce sync.Once
-
 // newSrvConfig - initialize a new server config, saves env parameters if
 // found, otherwise use default parameters
 func newSrvConfig(objAPI ObjectLayer) error {
@@ -579,9 +580,7 @@ func newSrvConfig(objAPI ObjectLayer) error {
 	srvCfg := newServerConfig()
 
 	// Override any values from ENVs.
-	lookupConfigOnce.Do(func() {
-		lookupConfigs(srvCfg)
-	})
+	lookupConfigs(srvCfg)
 
 	// hold the mutex lock before a new config is assigned.
 	globalServerConfigMu.Lock()
@@ -605,9 +604,7 @@ func loadConfig(objAPI ObjectLayer) error {
 	}
 
 	// Override any values from ENVs.
-	lookupConfigOnce.Do(func() {
-		lookupConfigs(srvCfg)
-	})
+	lookupConfigs(srvCfg)
 
 	// hold the mutex lock before a new config is assigned.
 	globalServerConfigMu.Lock()

cmd/config/etcd/dns/etcd_dns.go
@@ -47,6 +47,11 @@ func newCoreDNSMsg(ip string, port string, ttl uint32, t time.Time) ([]byte, err
 	})
 }
 
+// Close closes the internal etcd client and cannot be used further
+func (c *CoreDNS) Close() {
+	c.etcdClient.Close()
+}
+
 // List - Retrieves list of DNS entries for the domain.
 func (c *CoreDNS) List() (map[string][]SrvRecord, error) {
 	var srvRecords = map[string][]SrvRecord{}

cmd/gateway-main.go
@@ -162,9 +162,7 @@ func StartGateway(ctx *cli.Context, gw Gateway) {
 	srvCfg := newServerConfig()
 
 	// Override any values from ENVs.
-	lookupConfigOnce.Do(func() {
-		lookupConfigs(srvCfg)
-	})
+	lookupConfigs(srvCfg)
 
 	// hold the mutex lock before a new config is assigned.
 	globalServerConfigMu.Lock()