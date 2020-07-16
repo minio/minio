1341bf5a9e284c1764baf08c1ee7d2a6af0b4a02
鸿则 <hungtcs@163.com>
2020-07-17 01:46:27 +0800

    add preview width constraint (#10062)
    * fix: add preview width constraint
    
    * fix: set object item dropdown menu nowrap style

--- browser/app/js/objects/PreviewObjectModal.js
+++ browser/app/js/objects/PreviewObjectModal.js
@@ -46,7 +46,7 @@ class PreviewObjectModal extends React.Component {
         <ModalBody>
           <div className="input-group">
             {this.state.url && (
-              <object data={this.state.url}>
+              <object data={this.state.url} style={{ display: "block", width: "100%" }}>
                 <h3 style={{ textAlign: "center", display: "block", width: "100%" }}>
                   Do not have read permissions to preview "{this.props.object.name}"
                 </h3>
