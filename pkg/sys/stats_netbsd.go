7ce541dd672ed736bb718695ce927bedccc4f681
Matthias Petermann <mp@petermann-it.de>
2020-11-16 22:47:50 +0100

    NetBSD has two sysctls for reporting the physical memory.
    "hw.physmem" only reports reasonable results on 32 bit systems and is
    kept to not break binary compatibility. On other systems, it reports
    "-1". "hw.phymem64" reports reasonable results on all systems.
    
    Minio getHwPhysmem method currently uses the deprecated "hw.physmem"
    key which results in a parse error (due to the -1).
    
    This pull request asks for changing this. The change was tested
    successfully on NetBSD/amd64 9.1.

--- pkg/sys/stats_netbsd.go
+++ pkg/sys/stats_netbsd.go
@@ -19,27 +19,27 @@
 package sys
 
 import (
-    "encoding/binary"
-    "syscall"
+	"encoding/binary"
+	"syscall"
 )
 
 func getHwPhysmem() (uint64, error) {
-    totalString, err := syscall.Sysctl("hw.physmem64")
-    if err != nil {
-        return 0, err
-    }
+	totalString, err := syscall.Sysctl("hw.physmem64")
+	if err != nil {
+		return 0, err
+	}
 
-    // syscall.sysctl() helpfully assumes the result is a null-terminated string and
-    // removes the last byte of the result if it's 0 :/
-    totalString += "\x00"
+	// syscall.sysctl() helpfully assumes the result is a null-terminated string and
+	// removes the last byte of the result if it's 0 :/
+	totalString += "\x00"
 
-    total := uint64(binary.LittleEndian.Uint64([]byte(totalString)))
+	total := uint64(binary.LittleEndian.Uint64([]byte(totalString)))
 
-    return total, nil
+	return total, nil
 }
 
 // GetStats - return system statistics for bsd.
 func GetStats() (stats Stats, err error) {
-    stats.TotalRAM, err = getHwPhysmem()
-    return stats, err
+	stats.TotalRAM, err = getHwPhysmem()
+	return stats, err
 }
