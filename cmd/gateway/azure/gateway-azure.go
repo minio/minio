fbc5fc74108c19952bdf3a46e2e01929536c925e
Anis Elleuch <anis@min.io>
2020-11-20 13:29:44 +0100

    azure: Specify different Azure storage in the shell env
    AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY are used in az cli to specifiy
    the azure blob storage access & secret keys. With this commit, it is
    possible to set them if you want from the gateway own credentials to be
    different to the Azure blob credentials.

--- cmd/gateway/azure/gateway-azure.go
+++ cmd/gateway/azure/gateway-azure.go
@@ -29,6 +29,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
 	"sort"
 	"strconv"
@@ -173,6 +174,16 @@ func (g *Azure) Name() string {
 
 // NewGatewayLayer initializes azure blob storage client and returns AzureObjects.
 func (g *Azure) NewGatewayLayer(creds auth.Credentials) (minio.ObjectLayer, error) {
+	var err error
+
+	// Override credentials from the Azure storage environment variables if specified
+	if acc, key := os.Getenv("AZURE_STORAGE_ACCOUNT"), os.Getenv("AZURE_STORAGE_KEY"); acc != "" && key != "" {
+		creds, err = auth.CreateCredentials(acc, key)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	endpointURL, err := parseStorageEndpoint(g.host, creds.AccessKey)
 	if err != nil {
 		return nil, err
