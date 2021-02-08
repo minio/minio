9b10118d342cfdfad0742da0fcd6e0f80e1ad53e
Klaus Post <klauspost@gmail.com>
2021-02-08 11:36:16 -0800

    Metacache add abs entry limit (#11483)
    Add an absolute limit to the number of metacaches for a bucket.
    
    Delete excess caches if they haven't been handed out in an hour.

--- cmd/metacache-manager.go
+++ cmd/metacache-manager.go
@@ -43,6 +43,7 @@ type metacacheManager struct {
 }
 
 const metacacheManagerTransientBucket = "**transient**"
+const metacacheMaxEntries = 5000
 
 // initManager will start async saving the cache.
 func (m *metacacheManager) initManager() {
