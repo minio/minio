789bea2285b1ff50e6c2c5751f030fefd13d93cd
Harshavardhana <harsha@minio.io>
2021-05-05 20:38:58 -0700

    Apply suggestions from code review
    Co-authored-by: Klaus Post <klauspost@gmail.com>

--- pkg/dsync/rpc-client-interface.go
+++ pkg/dsync/rpc-client-interface.go
@@ -54,11 +54,15 @@ type NetLocker interface {
 	// Do read unlock for given LockArgs. It should return
 	// * a boolean to indicate success/failure of the operation
 	// * an error on failure of unlock request operation.
+	// Canceling the context will abort the remote call.
+	// In that case, the resource may or may not be unlocked.
 	RUnlock(ctx context.Context, args LockArgs) (bool, error)
 
 	// Do write unlock for given LockArgs. It should return
 	// * a boolean to indicate success/failure of the operation
 	// * an error on failure of unlock request operation.
+	// Canceling the context will abort the remote call.
+	// In that case, the resource may or may not be unlocked.
 	Unlock(ctx context.Context, args LockArgs) (bool, error)
 
 	// Refresh the given lock to prevent it from becoming stale
