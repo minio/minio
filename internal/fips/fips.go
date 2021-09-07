43d2655ee44935bedfa115442977a34d0f3280c3
Andreas Auernhammer <hi@aead.dev>
2021-09-07 18:57:32 +0200

    tls: add TLS 1.3 ciphers to the list of supported ciphers (#13158)
    This commit adds the TLS 1.3 ciphers to the list of
    supported ciphers. Now, clients can connect to MinIO
    using TLS 1.3
    
    Signed-off-by: Andreas Auernhammer <hi@aead.dev>

--- internal/fips/fips.go
+++ internal/fips/fips.go
@@ -34,6 +34,8 @@ func cipherSuitesDARE() []byte {
 
 func cipherSuitesTLS() []uint16 {
 	return []uint16{
+		tls.TLS_AES_128_GCM_SHA256,
+		tls.TLS_AES_256_GCM_SHA384,
 		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
