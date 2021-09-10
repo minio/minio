2fe6655c37552173e699b06e89070908d35cbf5b
ArthurMa <4406arthur@gmail.com>
2021-09-10 20:52:19 +0800

    Apply suggestions from code review
    Co-authored-by: Klaus Post <klauspost@gmail.com>

--- internal/logger/target/http/http.go
+++ internal/logger/target/http/http.go
@@ -98,7 +98,7 @@ func (h *Target) Init() error {
 	// Drain any response.
 	xhttp.DrainBody(resp.Body)
 
-	if !acceptedRespondStatusCode(resp.StatusCode) {
+	if !acceptedResponseStatusCode(resp.StatusCode) {
 		switch resp.StatusCode {
 		case http.StatusForbidden:
 			return fmt.Errorf("%s returned '%s', please check if your auth token is correctly set",
@@ -112,14 +112,11 @@ func (h *Target) Init() error {
 	return nil
 }
 
-//Accpet HTTP Statue Code
-var acceptedStatueCodeMap = map[int]bool{200: true, 201: true, 202: true}
+// Accepted HTTP Statue Codes
+var acceptedStatusCodeMap = map[int]bool{http.StatusOK: true, http.StatusCreated: true, http.StatusAccepted: true, http.StatusNoContent: true}
 
-func acceptedRespondStatusCode(code int) bool {
-	if _, ok := acceptedStatueCodeMap[code]; ok {
-		return true
-	}
-	return false
+func acceptedResponseStatusCode(code int) bool {
+	return acceptedStatusCodeMap[code]
 }
 
 func (h *Target) startHTTPLogger() {
