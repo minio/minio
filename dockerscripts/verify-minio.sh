39d51ce84513a45061906bda087b2443bc712522
Harshavardhana <harsha@minio.io>
2021-09-09 22:19:11 -0700

    fix: add Dockerfile.release* /opt/bin writable

--- dockerscripts/verify-minio.sh
+++ dockerscripts/verify-minio.sh
@@ -3,14 +3,14 @@
 
 set -e
 
-if [ ! -x "/usr/bin/minio" ]; then
+if [ ! -x "/opt/bin/minio" ]; then
     echo "minio executable binary not found refusing to proceed"
     exit 1
 fi
 
 verify_sha256sum() {
     echo "verifying binary checksum"
-    echo "$(awk '{print $1}' /usr/bin/minio.sha256sum)  /usr/bin/minio" | sha256sum -c
+    echo "$(awk '{print $1}' /opt/bin/minio.sha256sum)  /opt/bin/minio" | sha256sum -c
 }
 
 verify_signature() {
@@ -19,7 +19,7 @@ verify_signature() {
         return
     fi
     echo "verifying binary signature"
-    minisign -VQm /usr/bin/minio -P RWTx5Zr1tiHQLwG9keckT0c45M3AGeHD6IvimQHpyRywVWGbP1aVSGav
+    minisign -VQm /opt/bin/minio -P RWTx5Zr1tiHQLwG9keckT0c45M3AGeHD6IvimQHpyRywVWGbP1aVSGav
 }
 
 main() {
