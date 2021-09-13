5a64003f6f4ad96eee6282c5868d580f8303f88e
Klaus Post <klauspost@gmail.com>
2021-09-13 09:13:25 -0700

    select: Return null for non-exiting column indexes (#13196)
    Fixes #13186

--- internal/s3select/csv/record.go
+++ internal/s3select/csv/record.go
@@ -22,6 +22,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 
 	"github.com/bcicen/jstream"
 	csv "github.com/minio/csvparser"
@@ -43,6 +45,20 @@ type Record struct {
 func (r *Record) Get(name string) (*sql.Value, error) {
 	index, found := r.nameIndexMap[name]
 	if !found {
+		// Check if index.
+		if strings.HasPrefix(name, "_") {
+			idx, err := strconv.Atoi(strings.TrimPrefix(name, "_"))
+			if err != nil {
+				return nil, fmt.Errorf("column %v not found", name)
+			}
+			// The position count starts at 1.
+			idx--
+			if idx >= len(r.csvRecord) || idx < 0 {
+				// If field index > number of columns, return null
+				return sql.FromNull(), nil
+			}
+			return sql.FromBytes([]byte(r.csvRecord[idx])), nil
+		}
 		return nil, fmt.Errorf("column %v not found", name)
 	}
 
