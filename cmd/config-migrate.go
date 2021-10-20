ac36a377b0181b82b1114fe44baf553ce99e26b7
Harshavardhana <harsha@minio.io>
2021-10-20 11:31:09 -0700

    fix: remove deprecated jwks_url from config KV (#13477)

--- cmd/config-migrate.go
+++ cmd/config-migrate.go
@@ -2754,7 +2754,6 @@ func migrateMinioSysConfigToKV(objAPI ObjectLayer) error {
 	}
 
 	xldap.SetIdentityLDAP(newCfg, cfg.LDAPServerConfig)
-	openid.SetIdentityOpenID(newCfg, cfg.OpenID)
 	opa.SetPolicyOPAConfig(newCfg, cfg.Policy.OPA)
 	cache.SetCacheConfig(newCfg, cfg.Cache)
 	compress.SetCompressionConfig(newCfg, cfg.Compression)
