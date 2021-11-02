476ae0568afbd6228bd964e4a7cfbc57d0a3315f
Harshavardhana <harsha@minio.io>
2021-11-02 16:59:51 -0700

    fix: SQL select to honor limits properly for array queries
    added tests to cover the scenarios as well.

--- internal/s3select/sql/statement.go
+++ internal/s3select/sql/statement.go
@@ -302,9 +302,8 @@ func (e *SelectStatement) Eval(input, output Record) (Record, error) {
 		// .. WHERE ..`
 
 		// Update count of records output.
-		if e.limitValue > -1 {
-			e.outputCount++
-		}
+		e.outputCount++
+
 		return input.Clone(output), nil
 	}
 
@@ -328,9 +327,7 @@ func (e *SelectStatement) Eval(input, output Record) (Record, error) {
 	}
 
 	// Update count of records output.
-	if e.limitValue > -1 {
-		e.outputCount++
-	}
+	e.outputCount++
 
 	return output, nil
 }
