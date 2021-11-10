eba61d0ae89bbc3c9fbc5a4db9d4f4e544ae40ce
Harshavardhana <harsha@minio.io>
2021-11-10 12:41:55 -0800

    add multi-site replication tests

--- docs/bucket/replication/setup_3site_replication.sh
+++ docs/bucket/replication/setup_3site_replication.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-trap 'catch' ERR
+trap 'catch $LINENO' ERR
 
+# shellcheck disable=SC2120
 catch() {
+    if [ $# -ne 0 ]; then
+       echo "error on line $1"
+       for site in sitea siteb sitec; do
+           echo "$site server logs ========="
+           cat "/tmp/${site}_1.log"
+           echo "==========================="
+           cat "/tmp/${site}_2.log"
+       done
+    fi
+
     echo "Cleaning up instances of MinIO"
     pkill minio
+    pkill -9 minio
     rm -rf /tmp/multisitea
     rm -rf /tmp/multisiteb
     rm -rf /tmp/multisitec
@@ -13,47 +25,47 @@ catch() {
 catch
 
 set -e
-go install -v
 export MINIO_BROWSER=off
 export MINIO_ROOT_USER="minio"
 export MINIO_ROOT_PASSWORD="minio123"
 export MINIO_PROMETHEUS_AUTH_TYPE=public
+export PATH=${GOPATH}/bin:${PATH}
 
-minio server --address :9001 "http://localhost:9001/tmp/multisitea/data/disterasure/xl{1...4}" \
-      "http://localhost:9002/tmp/multisitea/data/disterasure/xl{5...8}" >/tmp/sitea_1.log 2>&1 &
-minio server --address :9002 "http://localhost:9001/tmp/multisitea/data/disterasure/xl{1...4}" \
-      "http://localhost:9002/tmp/multisitea/data/disterasure/xl{5...8}" >/tmp/sitea_2.log 2>&1 &
+minio server --address 127.0.0.1:9001 "http://127.0.0.1:9001/tmp/multisitea/data/disterasure/xl{1...4}" \
+      "http://127.0.0.1:9002/tmp/multisitea/data/disterasure/xl{5...8}" >/tmp/sitea_1.log 2>&1 &
+minio server --address 127.0.0.1:9002 "http://127.0.0.1:9001/tmp/multisitea/data/disterasure/xl{1...4}" \
+      "http://127.0.0.1:9002/tmp/multisitea/data/disterasure/xl{5...8}" >/tmp/sitea_2.log 2>&1 &
 
-minio server --address :9003 "http://localhost:9003/tmp/multisiteb/data/disterasure/xl{1...4}" \
-      "http://localhost:9004/tmp/multisiteb/data/disterasure/xl{5...8}" >/tmp/siteb_1.log 2>&1 &
-minio server --address :9004 "http://localhost:9003/tmp/multisiteb/data/disterasure/xl{1...4}" \
-      "http://localhost:9004/tmp/multisiteb/data/disterasure/xl{5...8}" >/tmp/siteb_2.log 2>&1 &
+minio server --address 127.0.0.1:9003 "http://127.0.0.1:9003/tmp/multisiteb/data/disterasure/xl{1...4}" \
+      "http://127.0.0.1:9004/tmp/multisiteb/data/disterasure/xl{5...8}" >/tmp/siteb_1.log 2>&1 &
+minio server --address 127.0.0.1:9004 "http://127.0.0.1:9003/tmp/multisiteb/data/disterasure/xl{1...4}" \
+      "http://127.0.0.1:9004/tmp/multisiteb/data/disterasure/xl{5...8}" >/tmp/siteb_2.log 2>&1 &
 
-minio server --address :9005 "http://localhost:9005/tmp/multisitec/data/disterasure/xl{1...4}" \
-      "http://localhost:9006/tmp/multisitec/data/disterasure/xl{5...8}" >/tmp/sitec_1.log 2>&1 &
-minio server --address :9006 "http://localhost:9005/tmp/multisitec/data/disterasure/xl{1...4}" \
-      "http://localhost:9006/tmp/multisitec/data/disterasure/xl{5...8}" >/tmp/sitec_2.log 2>&1 &
+minio server --address 127.0.0.1:9005 "http://127.0.0.1:9005/tmp/multisitec/data/disterasure/xl{1...4}" \
+      "http://127.0.0.1:9006/tmp/multisitec/data/disterasure/xl{5...8}" >/tmp/sitec_1.log 2>&1 &
+minio server --address 127.0.0.1:9006 "http://127.0.0.1:9005/tmp/multisitec/data/disterasure/xl{1...4}" \
+      "http://127.0.0.1:9006/tmp/multisitec/data/disterasure/xl{5...8}" >/tmp/sitec_2.log 2>&1 &
 
 sleep 30
 
-mc alias set sitea http://localhost:9001 minio minio123
+mc alias set sitea http://127.0.0.1:9001 minio minio123
 mc mb sitea/bucket
 mc version enable sitea/bucket
 mc mb -l sitea/olockbucket
 
-mc alias set siteb http://localhost:9004 minio minio123
+mc alias set siteb http://127.0.0.1:9004 minio minio123
 mc mb siteb/bucket/
 mc version enable siteb/bucket/
 mc mb -l siteb/olockbucket/
 
-mc alias set sitec http://localhost:9006 minio minio123
+mc alias set sitec http://127.0.0.1:9006 minio minio123
 mc mb sitec/bucket/
 mc version enable sitec/bucket/
 mc mb -l sitec/olockbucket
 
 echo "adding replication config for site a -> site b"
 remote_arn=$(mc admin bucket remote add sitea/bucket/ \
-   http://minio:minio123@localhost:9004/bucket \
+   http://minio:minio123@127.0.0.1:9004/bucket \
    --service "replication" --json | jq -r ".RemoteARN")
 echo "adding replication rule for a -> b : ${remote_arn}"
 sleep 1
@@ -64,7 +76,7 @@ sleep 1
 
 echo "adding replication config for site b -> site a"
 remote_arn=$(mc admin bucket remote add siteb/bucket/ \
-   http://minio:minio123@localhost:9001/bucket \
+   http://minio:minio123@127.0.0.1:9001/bucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for b -> a : ${remote_arn}"
@@ -75,7 +87,7 @@ sleep 1
 
 echo "adding replication config for site a -> site c"
 remote_arn=$(mc admin bucket remote add sitea/bucket/ \
-   http://minio:minio123@localhost:9006/bucket \
+   http://minio:minio123@127.0.0.1:9006/bucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for a -> c : ${remote_arn}"
@@ -85,7 +97,7 @@ mc replicate add sitea/bucket/ \
 sleep 1
 echo "adding replication config for site c -> site a"
 remote_arn=$(mc admin bucket remote add sitec/bucket/ \
-   http://minio:minio123@localhost:9001/bucket \
+   http://minio:minio123@127.0.0.1:9001/bucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for c -> a : ${remote_arn}"
@@ -95,7 +107,7 @@ mc replicate add sitec/bucket/ \
 sleep 1
 echo "adding replication config for site b -> site c"
 remote_arn=$(mc admin bucket remote add siteb/bucket/ \
-   http://minio:minio123@localhost:9006/bucket \
+   http://minio:minio123@127.0.0.1:9006/bucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for b -> c : ${remote_arn}"
@@ -106,7 +118,7 @@ sleep 1
 
 echo "adding replication config for site c -> site b"
 remote_arn=$(mc admin bucket remote add sitec/bucket \
-   http://minio:minio123@localhost:9004/bucket \
+   http://minio:minio123@127.0.0.1:9004/bucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for c -> b : ${remote_arn}"
@@ -116,7 +128,7 @@ mc replicate add sitec/bucket/ \
 sleep 1
 echo "adding replication config for olockbucket site a -> site b"
 remote_arn=$(mc admin bucket remote add sitea/olockbucket/ \
-   http://minio:minio123@localhost:9004/olockbucket \
+   http://minio:minio123@127.0.0.1:9004/olockbucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for olockbucket a -> b : ${remote_arn}"
@@ -126,7 +138,7 @@ mc replicate add sitea/olockbucket/ \
 sleep 1
 echo "adding replication config for site b -> site a"
 remote_arn=$(mc admin bucket remote add siteb/olockbucket/ \
-   http://minio:minio123@localhost:9001/olockbucket \
+   http://minio:minio123@127.0.0.1:9001/olockbucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for olockbucket b -> a : ${remote_arn}"
@@ -136,7 +148,7 @@ mc replicate add siteb/olockbucket/ \
 sleep 1
 echo "adding replication config for olockbucket site a -> site c"
 remote_arn=$(mc admin bucket remote add sitea/olockbucket/ \
-   http://minio:minio123@localhost:9006/olockbucket \
+   http://minio:minio123@127.0.0.1:9006/olockbucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for olockbucket a -> c : ${remote_arn}"
@@ -146,7 +158,7 @@ mc replicate add sitea/olockbucket/ \
 sleep 1
 echo "adding replication config for site c -> site a"
 remote_arn=$(mc admin bucket remote add sitec/olockbucket/ \
-   http://minio:minio123@localhost:9001/olockbucket \
+   http://minio:minio123@127.0.0.1:9001/olockbucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for olockbucket c -> a : ${remote_arn}"
@@ -156,7 +168,7 @@ mc replicate add sitec/olockbucket/ \
 sleep 1
 echo "adding replication config for site b -> site c"
 remote_arn=$(mc admin bucket remote add siteb/olockbucket/ \
-   http://minio:minio123@localhost:9006/olockbucket \
+   http://minio:minio123@127.0.0.1:9006/olockbucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for olockbucket b -> c : ${remote_arn}"
@@ -166,7 +178,7 @@ mc replicate add siteb/olockbucket/ \
 sleep 1
 echo "adding replication config for site c -> site b"
 remote_arn=$(mc admin bucket remote add sitec/olockbucket \
-   http://minio:minio123@localhost:9004/olockbucket \
+   http://minio:minio123@127.0.0.1:9004/olockbucket \
    --service "replication" --json | jq -r ".RemoteARN")
 sleep 1
 echo "adding replication rule for olockbucket c -> b : ${remote_arn}"
