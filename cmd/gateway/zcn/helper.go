13c1143d1c6bb3e507664b25f17cba42d623c38f
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2021-12-22 07:51:03 +0545

    Comply with make verifiers

--- cmd/gateway/zcn/helper.go
+++ cmd/gateway/zcn/helper.go
@@ -10,6 +10,7 @@ func newMinioReader(source io.Reader) *MinioReader {
 	return &MinioReader{source}
 }
 
+// MinioReader Reader that returns io.EOF for io.ErrUnexpectedEOF error
 type MinioReader struct {
 	io.Reader
 }
@@ -25,13 +26,13 @@ func (r *MinioReader) Read(p []byte) (n int, err error) {
 
 func getTimeOut(size uint64) time.Duration {
 	switch {
-	case size >= OneGB:
+	case size >= oneGB:
 		return time.Minute * 30
-	case size >= 500*OneMB:
+	case size >= 500*oneMB:
 		return time.Minute * 5
-	case size >= HundredMB:
+	case size >= hundredMB:
 		return time.Minute * 3
-	case size >= 50*OneMB:
+	case size >= 50*oneMB:
 		return time.Minute
 	default:
 		return time.Second * 30
