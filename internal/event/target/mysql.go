minio/minio

commit 91a0e7bdaa75b51ebbf06502ea0fd035b42ddee5

update mysql notification key length, character set and collation (#13414)
fixes #13227

internal/event/target/mysql.go
@@ -37,8 +37,10 @@ import (
 
 const (
 	mysqlTableExists          = `SELECT 1 FROM %s;`
-	mysqlCreateNamespaceTable = `CREATE TABLE %s (key_name VARCHAR(2048), value JSON, PRIMARY KEY (key_name));`
-	mysqlCreateAccessTable    = `CREATE TABLE %s (event_time DATETIME NOT NULL, event_data JSON);`
+	mysqlCreateNamespaceTable = `CREATE TABLE %s (key_name VARCHAR(4096), value JSON, PRIMARY KEY (key_name))
+                                       CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;`
+	mysqlCreateAccessTable = `CREATE TABLE %s (event_time DATETIME NOT NULL, event_data JSON)
+                                    ROW_FORMAT = Dynamic;`
 
 	mysqlUpdateRow = `INSERT INTO %s (key_name, value) VALUES (?, ?) ON DUPLICATE KEY UPDATE value=VALUES(value);`
 	mysqlDeleteRow = `DELETE FROM %s WHERE key_name = ?;`

commit 9f9f35e54a6bd578f30b7e2875e58226fd2e8170

Limit key size to 3K
User is reporting `Error 1071 :Specified key was too long,max key length is 3072 bytes`.

Regression caused by #13414

internal/event/target/mysql.go
@@ -36,8 +36,9 @@ import (
 )
 
 const (
-	mysqlTableExists          = `SELECT 1 FROM %s;`
-	mysqlCreateNamespaceTable = `CREATE TABLE %s (key_name VARCHAR(4096), value JSON, PRIMARY KEY (key_name))
+	mysqlTableExists = `SELECT 1 FROM %s;`
+	// Some MySQL has a 3072 byte limit on key sizes.
+	mysqlCreateNamespaceTable = `CREATE TABLE %s (key_name VARCHAR(3072), value JSON, PRIMARY KEY (key_name))
                                        CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;`
 	mysqlCreateAccessTable = `CREATE TABLE %s (event_time DATETIME NOT NULL, event_data JSON)
                                     ROW_FORMAT = Dynamic;`