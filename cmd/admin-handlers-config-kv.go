a07e7458ac6485fde1f8e1e49d6dad17614971df
Poorna Krishnamoorthy <poorna@minio.io>
2022-01-27 16:19:12 -0800

    Validate config before saving changes after config reset

--- cmd/admin-handlers-config-kv.go
+++ cmd/admin-handlers-config-kv.go
@@ -74,6 +74,10 @@ func (a adminAPIHandlers) DelConfigKVHandler(w http.ResponseWriter, r *http.Requ
 		writeErrorResponseJSON(ctx, w, toAdminAPIErr(ctx, err), r.URL)
 		return
 	}
+	if err = validateConfig(cfg); err != nil {
+		writeCustomErrorResponseJSON(ctx, w, errorCodes.ToAPIErr(ErrAdminConfigBadJSON), err.Error(), r.URL)
+		return
+	}
 
 	if err = saveServerConfig(ctx, objectAPI, cfg); err != nil {
 		writeErrorResponseJSON(ctx, w, toAdminAPIErr(ctx, err), r.URL)
