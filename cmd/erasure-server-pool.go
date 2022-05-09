13be97d91bb7fe4a018dfd38d33cb1df23875c7d
Anis Elleuch <anis@min.io>
2022-05-09 23:53:56 +0100

    pools: GetObjectNInfo should cover locking during object read
    In case of multi zones setup, GetObjectNInfo returns a GetObjectReader
    but it unlocks the read lock when quitting GetObjectNInfo. This should
    not happen, unlock should only happen when GetObjectReader is closed.

--- cmd/erasure-server-pool.go
+++ cmd/erasure-server-pool.go
@@ -774,7 +774,13 @@ func (z *erasureServerPools) GetObjectNInfo(ctx context.Context, bucket, object 
 	}
 
 	lockType = noLock // do not take locks at lower levels for GetObjectNInfo()
-	return z.serverPools[zIdx].GetObjectNInfo(ctx, bucket, object, rs, h, lockType, opts)
+	gr, err = z.serverPools[zIdx].GetObjectNInfo(ctx, bucket, object, rs, h, lockType, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	unlockOnDefer = false
+	return gr.WithCleanupFuncs(nsUnlocker), nil
 }
 
 // getLatestObjectInfoWithIdx returns the objectInfo of the latest object from multiple pools (this function
