233b06da64a29b2b9dd0a60e9672de7de2b80824
Andreas Auernhammer <hi@aead.dev>
2022-06-21 11:24:03 +0200

    fips: simplify TLS configuration
    This commit simplifies the TLS configuration.
    It inlines the FIPS / non-FIPS code.
    
    Signed-off-by: Andreas Auernhammer <hi@aead.dev>

--- internal/fips/fips.go
+++ internal/fips/fips.go
@@ -20,29 +20,4 @@
 
 package fips
 
-import (
-	"crypto/tls"
-
-	"github.com/minio/sio"
-)
-
 const enabled = true
-
-func cipherSuitesDARE() []byte {
-	return []byte{sio.AES_256_GCM}
-}
-
-func cipherSuitesTLS() []uint16 {
-	return []uint16{
-		tls.TLS_AES_128_GCM_SHA256,
-		tls.TLS_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-	}
-}
-
-func ellipticCurvesTLS() []tls.CurveID {
-	return []tls.CurveID{tls.CurveP256}
-}
