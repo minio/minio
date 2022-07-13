8a30a505b1d47b47597f5cb7682e00a573d71187
Klaus Post <klauspost@gmail.com>
2022-07-13 13:33:03 +0200

    Don't pad for test.

--- cmd/object-api-utils_test.go
+++ cmd/object-api-utils_test.go
@@ -624,7 +624,7 @@ func TestS2CompressReader(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			buf := make([]byte, 100) // make small buffer to ensure multiple reads are required for large case
 
-			r, idxCB := newS2CompressReader(bytes.NewReader(tt.data), int64(len(tt.data)), true)
+			r, idxCB := newS2CompressReader(bytes.NewReader(tt.data), int64(len(tt.data)), false)
 			defer r.Close()
 
 			var rdrBuf bytes.Buffer
