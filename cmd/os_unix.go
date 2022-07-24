7725425e051b15d8bb3696ce5b5208c8276f50b9
Harshavardhana <harsha@minio.io>
2022-07-24 00:43:11 -0700

    fix: fork os.MkdirAll to optimize cases where parent exists (#15379)
    a/b/c/d/ where `a/b/c/` exists results in additional syscalls
    such as an Lstat() call to verify if the `a/b/c/` exists
    and its a directory.
    
    We do not need to do this on MinIO since the parent prefixes
    if exist, we can simply return success without spending
    additional syscalls.
    
    Also this implementation attempts to simply use Access() calls
    to avoid os.Stat() calls since the latter does memory allocation
    for things we do not need to use.
    
    Access() is simpler since we have a predictable structure on
    the backend and we know exactly how our path structures are.

--- cmd/os_unix.go
+++ cmd/os_unix.go
@@ -38,6 +38,55 @@ func access(name string) error {
 	return nil
 }
 
+// Forked from Golang but chooses fast path upon os.Mkdir()
+// error to avoid os.Lstat() call.
+//
+// osMkdirAll creates a directory named path,
+// along with any necessary parents, and returns nil,
+// or else returns an error.
+// The permission bits perm (before umask) are used for all
+// directories that MkdirAll creates.
+// If path is already a directory, MkdirAll does nothing
+// and returns nil.
+func osMkdirAll(dirPath string, perm os.FileMode) error {
+	// Fast path: if we can tell whether path is a directory or file, stop with success or error.
+	err := Access(dirPath)
+	if err == nil {
+		return nil
+	}
+	if !osIsNotExist(err) {
+		return &os.PathError{Op: "mkdir", Path: dirPath, Err: err}
+	}
+
+	// Slow path: make sure parent exists and then call Mkdir for path.
+	i := len(dirPath)
+	for i > 0 && os.IsPathSeparator(dirPath[i-1]) { // Skip trailing path separator.
+		i--
+	}
+
+	j := i
+	for j > 0 && !os.IsPathSeparator(dirPath[j-1]) { // Scan backward over element.
+		j--
+	}
+
+	if j > 1 {
+		// Create parent.
+		if err = osMkdirAll(dirPath[:j-1], perm); err != nil {
+			return err
+		}
+	}
+
+	// Parent now exists; invoke Mkdir and use its result.
+	if err = Mkdir(dirPath, perm); err != nil {
+		if osIsExist(err) {
+			return nil
+		}
+		return err
+	}
+
+	return nil
+}
+
 // The buffer must be at least a block long.
 // refer https://github.com/golang/go/issues/24015
 const blockSize = 8 << 10 // 8192
--- cmd/osmetric_string.go
+++ cmd/osmetric_string.go
@@ -10,24 +10,25 @@ func _() {
 	var x [1]struct{}
 	_ = x[osMetricRemoveAll-0]
 	_ = x[osMetricMkdirAll-1]
-	_ = x[osMetricRename-2]
-	_ = x[osMetricOpenFile-3]
-	_ = x[osMetricOpen-4]
-	_ = x[osMetricOpenFileDirectIO-5]
-	_ = x[osMetricLstat-6]
-	_ = x[osMetricRemove-7]
-	_ = x[osMetricStat-8]
-	_ = x[osMetricAccess-9]
-	_ = x[osMetricCreate-10]
-	_ = x[osMetricReadDirent-11]
-	_ = x[osMetricFdatasync-12]
-	_ = x[osMetricSync-13]
-	_ = x[osMetricLast-14]
+	_ = x[osMetricMkdir-2]
+	_ = x[osMetricRename-3]
+	_ = x[osMetricOpenFile-4]
+	_ = x[osMetricOpen-5]
+	_ = x[osMetricOpenFileDirectIO-6]
+	_ = x[osMetricLstat-7]
+	_ = x[osMetricRemove-8]
+	_ = x[osMetricStat-9]
+	_ = x[osMetricAccess-10]
+	_ = x[osMetricCreate-11]
+	_ = x[osMetricReadDirent-12]
+	_ = x[osMetricFdatasync-13]
+	_ = x[osMetricSync-14]
+	_ = x[osMetricLast-15]
 }
 
-const _osMetric_name = "RemoveAllMkdirAllRenameOpenFileOpenOpenFileDirectIOLstatRemoveStatAccessCreateReadDirentFdatasyncSyncLast"
+const _osMetric_name = "RemoveAllMkdirAllMkdirRenameOpenFileOpenOpenFileDirectIOLstatRemoveStatAccessCreateReadDirentFdatasyncSyncLast"
 
-var _osMetric_index = [...]uint8{0, 9, 17, 23, 31, 35, 51, 56, 62, 66, 72, 78, 88, 97, 101, 105}
+var _osMetric_index = [...]uint8{0, 9, 17, 22, 28, 36, 40, 56, 61, 67, 71, 77, 83, 93, 102, 106, 110}
 
 func (i osMetric) String() string {
 	if i >= osMetric(len(_osMetric_index)-1) {
