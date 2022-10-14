b7eaf90224030f3e7d4642d19013f3121ef2590c
jiuker <2818723467@qq.com>
2022-10-14 17:49:17 +0800

    Update endpoint-ellipses.go
    This is a strange syntax, does it need to be fixed for this strange usage?
    ```go
    package main
    
    import (
    	"fmt"
    )
    
    func main() {
    	a := A{}
    	for i := 0; i < 100; i++ {
    		a.Change(i)
    	}
    	a.Say()
    }
    
    type A struct {
    	V []int
    }
    
    func (a A) Change(v int) {
    	a.V = append(a.V, v)
    }
    
    func (a A) Say() {
    	fmt.Println(a.V)
    }
    ```

--- cmd/endpoint-ellipses.go
+++ cmd/endpoint-ellipses.go
@@ -207,7 +207,7 @@ func getSetIndexes(args []string, totalSizes []uint64, customSetDriveCount uint6
 }
 
 // Returns all the expanded endpoints, each argument is expanded separately.
-func (s endpointSet) getEndpoints() (endpoints []string) {
+func (s *endpointSet) getEndpoints() (endpoints []string) {
 	if len(s.endpoints) != 0 {
 		return s.endpoints
 	}
