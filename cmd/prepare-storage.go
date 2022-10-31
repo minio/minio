6c4421172a1107a3cdfb70d78c4596fcf280f689
Klaus Post <klauspost@gmail.com>
2022-10-31 12:35:09 +0100

    Clean entire tmp-old on restart
    In cases where the server is restarted before the previous run had finished would leak the data.
    
    Do a removeAll on the entire folder. This may clash a bit, but will ensure that the path is cleaned on every startup. There is no logging in the subpaths, so it shouldn't be noisy.
    
    To mitigate this somewhat we only do cleanup when health checks are enabled and for the non-rest instances.

--- cmd/prepare-storage.go
+++ cmd/prepare-storage.go
@@ -97,7 +97,8 @@ func bgFormatErasureCleanupTmp(diskPath string) {
 			err))
 	}
 
-	go removeAll(tmpOld)
+	// Remove the entire folder in case there are leftovers that didn't get cleaned up before restart.
+	go removeAll(pathJoin(diskPath, minioMetaTmpBucket+"-old"))
 	// Renames and schedules for purging all bucket metacache.
 	go renameAllBucketMetacache(diskPath)
 }
@@ -146,7 +147,7 @@ func isServerResolvable(endpoint Endpoint, timeout time.Duration) error {
 // time. additionally make sure to close all the disks used in this attempt.
 func connectLoadInitFormats(verboseLogging bool, firstDisk bool, endpoints Endpoints, poolCount, setCount, setDriveCount int, deploymentID, distributionAlgo string) (storageDisks []StorageAPI, format *formatErasureV3, err error) {
 	// Initialize all storage disks
-	storageDisks, errs := initStorageDisksWithErrors(endpoints)
+	storageDisks, errs := initStorageDisksWithErrors(endpoints, true)
 
 	defer func(storageDisks []StorageAPI) {
 		if err != nil {
