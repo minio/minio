26767909f30051bfd233204324cacae358bb2160
Nicolas <nicolasleblanc27@gmail.com>
2022-12-01 11:31:16 -0500

    remove duplicate error check

--- cmd/fs-v1.go
+++ cmd/fs-v1.go
@@ -974,9 +974,6 @@ func (fs *FSObjects) getObjectInfo(ctx context.Context, bucket, object string) (
 			return ObjectInfo{}, toObjectErr(err, bucket, object)
 		}
 		id = gateway.GetId(readCloser)
-		if err != nil {
-			return ObjectInfo{}, toObjectErr(err, bucket, object)
-		}
 	}
 
 	return fsMeta.ToObjectInfo(bucket, object, fi, id), nil
