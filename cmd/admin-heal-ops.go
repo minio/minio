62c4fea6360fe4429bce443e3f5ac753716d3c4e
jiuker <2818723467@qq.com>
2022-12-06 17:54:38 +0800

    Update admin-heal-ops.go

--- cmd/admin-heal-ops.go
+++ cmd/admin-heal-ops.go
@@ -235,8 +235,8 @@ func (ahs *allHealState) getHealSequenceByToken(token string) (h *healSequence, 
 // getHealSequence - Retrieve a heal sequence by path. The second
 // argument returns if a heal sequence actually exists.
 func (ahs *allHealState) getHealSequence(path string) (h *healSequence, exists bool) {
-	ahs.Lock()
-	defer ahs.Unlock()
+	ahs.RLock()
+	defer ahs.RUnlock()
 	h, exists = ahs.healSeqMap[path]
 	return h, exists
 }
