4b1b212b9e4bff4632ef0d4bcb1fcfdaeaf8995c
jiuker <2818723467@qq.com>
2022-12-06 17:41:28 +0800

    use readlock instead of writelock
    use readlock instead of writelock

--- cmd/admin-heal-ops.go
+++ cmd/admin-heal-ops.go
@@ -222,8 +222,8 @@ func (ahs *allHealState) periodicHealSeqsClean(ctx context.Context) {
 // getHealSequenceByToken - Retrieve a heal sequence by token. The second
 // argument returns if a heal sequence actually exists.
 func (ahs *allHealState) getHealSequenceByToken(token string) (h *healSequence, exists bool) {
-	ahs.Lock()
-	defer ahs.Unlock()
+	ahs.RLock()
+	defer ahs.RUnlock()
 	for _, healSeq := range ahs.healSeqMap {
 		if healSeq.clientToken == token {
 			return healSeq, true
