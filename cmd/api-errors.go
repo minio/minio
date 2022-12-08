cb09fe048d96db265943aa0eff241f7e93e1ff4d
Harshavardhana <harsha@minio.io>
2022-12-08 11:00:13 -0800

    remove hard limit for number of buckets
    instead it is a recommendation now, and
    we log to indicate there is a potential
    problem.

--- cmd/api-errors.go
+++ cmd/api-errors.go
@@ -177,7 +177,6 @@ const (
 	ErrBucketAlreadyOwnedByYou
 	ErrInvalidDuration
 	ErrBucketAlreadyExists
-	ErrTooManyBuckets
 	ErrMetadataTooLarge
 	ErrUnsupportedMetadata
 	ErrMaximumExpires
@@ -693,11 +692,6 @@ var errorCodes = errorCodeMap{
 	ErrSignatureVersionNotSupported: {
 		Code:           "InvalidRequest",
 		Description:    "The authorization mechanism you have provided is not supported. Please use AWS4-HMAC-SHA256.",
-		HTTPStatusCode: http.StatusBadRequest,
-	},
-	ErrTooManyBuckets: {
-		Code:           "TooManyBuckets",
-		Description:    "You have attempted to create more buckets than allowed",
 		HTTPStatusCode: http.StatusBadRequest,
 	},
 	ErrBucketNotEmpty: {
