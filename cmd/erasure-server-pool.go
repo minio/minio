362d8137bcca20feee19e7109603c2fdcbf81060
guozhi.li <guozhi.li@daocloud.io>
2023-01-13 14:28:05 +0800

    make it right

--- cmd/erasure-server-pool.go
+++ cmd/erasure-server-pool.go
@@ -187,10 +187,8 @@ func (z *erasureServerPools) GetDisksID(ids ...string) []StorageAPI {
 	}
 	res := make([]StorageAPI, 0, len(idMap))
 	for _, s := range z.serverPools {
-		s.erasureDisksMu.RLock()
-		defer s.erasureDisksMu.RUnlock()
-		for _, disks := range s.erasureDisks {
-			for _, disk := range disks {
+		for _, set := range s.sets {
+			for _, disk := range set.getDisks() {
 				if disk == OfflineDisk {
 					continue
 				}
@@ -213,8 +211,8 @@ func (z *erasureServerPools) GetRawData(ctx context.Context, volume, file string
 	for _, s := range z.serverPools {
 		s.erasureDisksMu.RLock()
 		defer s.erasureDisksMu.RUnlock()
-		for _, disks := range s.erasureDisks {
-			for _, disk := range disks {
+		for _, set := range s.sets {
+			for _, disk := range set.getDisks() {
 				if disk == OfflineDisk {
 					continue
 				}
