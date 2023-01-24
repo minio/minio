9820165cf43d91fd5d22eea3e1b7ff0c618ced49
Anis Elleuch <anis@min.io>
2023-01-24 11:55:15 +0100

    Print golang http errors in MinIO log format
    Some errors are important to see, such as Accept new connection
    failures. This might generate more messages than we want but we can
    disable them in the future.

--- cmd/server-main.go
+++ cmd/server-main.go
@@ -589,7 +589,7 @@ func serverMain(ctx *cli.Context) {
 		UseIdleTimeout(ctx.Duration("idle-timeout")).
 		UseReadHeaderTimeout(ctx.Duration("read-header-timeout")).
 		UseBaseContext(GlobalContext).
-		UseCustomLogger(log.New(io.Discard, "", 0)) // Turn-off random logging by Go stdlib
+		UseCustomLogger(log.New(&goHTTPLogger{}, "", 0))
 
 	go func() {
 		globalHTTPServerErrorCh <- httpServer.Start(GlobalContext)
