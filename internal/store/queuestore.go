51f7f9aaa38f23d8de5ba606eb08836cfcf1370e
Praveen raj Mani <praveen@minio.io>
2023-04-04 23:22:24 +0530

    Generalize the event store using go generics (#16910)

--- internal/store/queuestore.go
+++ internal/store/queuestore.go
@@ -15,10 +15,11 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package target
+package store
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 	"path/filepath"
 	"sort"
@@ -26,38 +27,47 @@ import (
 	"sync"
 	"time"
 
-	"github.com/minio/minio/internal/event"
+	"github.com/google/uuid"
 )
 
 const (
 	defaultLimit = 100000 // Default store limit.
-	eventExt     = ".event"
+	defaultExt   = ".unknown"
 )
 
-// QueueStore - Filestore for persisting events.
-type QueueStore struct {
+// errLimitExceeded error is sent when the maximum limit is reached.
+var errLimitExceeded = errors.New("the maximum store limit reached")
+
+// QueueStore - Filestore for persisting items.
+type QueueStore[_ any] struct {
 	sync.RWMutex
 	entryLimit uint64
 	directory  string
+	fileExt    string
 
 	entries map[string]int64 // key -> modtime as unix nano
 }
 
 // NewQueueStore - Creates an instance for QueueStore.
-func NewQueueStore(directory string, limit uint64) Store {
+func NewQueueStore[I any](directory string, limit uint64, ext string) *QueueStore[I] {
 	if limit == 0 {
 		limit = defaultLimit
 	}
 
-	return &QueueStore{
+	if ext == "" {
+		ext = defaultExt
+	}
+
+	return &QueueStore[I]{
 		directory:  directory,
 		entryLimit: limit,
+		fileExt:    ext,
 		entries:    make(map[string]int64, limit),
 	}
 }
 
 // Open - Creates the directory if not present.
-func (store *QueueStore) Open() error {
+func (store *QueueStore[_]) Open() error {
 	store.Lock()
 	defer store.Unlock()
 
@@ -78,7 +88,7 @@ func (store *QueueStore) Open() error {
 		if file.IsDir() {
 			continue
 		}
-		key := strings.TrimSuffix(file.Name(), eventExt)
+		key := strings.TrimSuffix(file.Name(), store.fileExt)
 		if fi, err := file.Info(); err == nil {
 			store.entries[key] = fi.ModTime().UnixNano()
 		}
@@ -87,44 +97,45 @@ func (store *QueueStore) Open() error {
 	return nil
 }
 
-// write - writes event to the directory.
-func (store *QueueStore) write(key string, e event.Event) error {
-	// Marshalls the event.
-	eventData, err := json.Marshal(e)
+// write - writes an item to the directory.
+func (store *QueueStore[I]) write(key string, item I) error {
+	// Marshalls the item.
+	eventData, err := json.Marshal(item)
 	if err != nil {
 		return err
 	}
 
-	path := filepath.Join(store.directory, key+eventExt)
+	path := filepath.Join(store.directory, key+store.fileExt)
 	if err := os.WriteFile(path, eventData, os.FileMode(0o770)); err != nil {
 		return err
 	}
 
-	// Increment the event count.
+	// Increment the item count.
 	store.entries[key] = time.Now().UnixNano()
 
 	return nil
 }
 
-// Put - puts a event to the store.
-func (store *QueueStore) Put(e event.Event) error {
+// Put - puts an item to the store.
+func (store *QueueStore[I]) Put(item I) error {
 	store.Lock()
 	defer store.Unlock()
 	if uint64(len(store.entries)) >= store.entryLimit {
 		return errLimitExceeded
 	}
-	key, err := getNewUUID()
+	// Generate a new UUID for the key.
+	key, err := uuid.NewRandom()
 	if err != nil {
 		return err
 	}
-	return store.write(key, e)
+	return store.write(key.String(), item)
 }
 
-// Get - gets a event from the store.
-func (store *QueueStore) Get(key string) (event event.Event, err error) {
+// Get - gets an item from the store.
+func (store *QueueStore[I]) Get(key string) (item I, err error) {
 	store.RLock()
 
-	defer func(store *QueueStore) {
+	defer func(store *QueueStore[I]) {
 		store.RUnlock()
 		if err != nil {
 			// Upon error we remove the entry.
@@ -133,31 +144,31 @@ func (store *QueueStore) Get(key string) (event event.Event, err error) {
 	}(store)
 
 	var eventData []byte
-	eventData, err = os.ReadFile(filepath.Join(store.directory, key+eventExt))
+	eventData, err = os.ReadFile(filepath.Join(store.directory, key+store.fileExt))
 	if err != nil {
-		return event, err
+		return item, err
 	}
 
 	if len(eventData) == 0 {
-		return event, os.ErrNotExist
+		return item, os.ErrNotExist
 	}
 
-	if err = json.Unmarshal(eventData, &event); err != nil {
-		return event, err
+	if err = json.Unmarshal(eventData, &item); err != nil {
+		return item, err
 	}
 
-	return event, nil
+	return item, nil
 }
 
 // Del - Deletes an entry from the store.
-func (store *QueueStore) Del(key string) error {
+func (store *QueueStore[_]) Del(key string) error {
 	store.Lock()
 	defer store.Unlock()
 	return store.del(key)
 }
 
 // Len returns the entry count.
-func (store *QueueStore) Len() int {
+func (store *QueueStore[_]) Len() int {
 	store.RLock()
 	l := len(store.entries)
 	defer store.RUnlock()
@@ -165,8 +176,8 @@ func (store *QueueStore) Len() int {
 }
 
 // lockless call
-func (store *QueueStore) del(key string) error {
-	err := os.Remove(filepath.Join(store.directory, key+eventExt))
+func (store *QueueStore[_]) del(key string) error {
+	err := os.Remove(filepath.Join(store.directory, key+store.fileExt))
 
 	// Delete as entry no matter the result
 	delete(store.entries, key)
@@ -175,7 +186,7 @@ func (store *QueueStore) del(key string) error {
 }
 
 // List - lists all files registered in the store.
-func (store *QueueStore) List() ([]string, error) {
+func (store *QueueStore[_]) List() ([]string, error) {
 	store.RLock()
 	l := make([]string, 0, len(store.entries))
 	for k := range store.entries {
@@ -194,7 +205,7 @@ func (store *QueueStore) List() ([]string, error) {
 // list will read all entries from disk.
 // Entries are returned sorted by modtime, oldest first.
 // Underlying entry list in store is *not* updated.
-func (store *QueueStore) list() ([]os.DirEntry, error) {
+func (store *QueueStore[_]) list() ([]os.DirEntry, error) {
 	files, err := os.ReadDir(store.directory)
 	if err != nil {
 		return nil, err
@@ -214,4 +225,10 @@ func (store *QueueStore) list() ([]os.DirEntry, error) {
 	})
 
 	return files, nil
+}
+
+// Extension will return the file extension used
+// for the items stored in the queue.
+func (store *QueueStore[_]) Extension() string {
+	return store.fileExt
 }
