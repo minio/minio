4a425cbac1ab0cb641cd6d00cc8cc51e4bd552f4
Harshavardhana <harsha@minio.io>
2023-05-25 22:07:25 -0700

    cleanup scripts and apply shfmt (#17284)

--- docs/distributed/decom.sh
+++ docs/distributed/decom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$TEST_DEBUG" ]; then
-    set -x
+	set -x
 fi
 
 pkill minio
@@ -9,13 +9,13 @@ rm -rf /tmp/xl
 rm -rf /tmp/xltier
 
 if [ ! -f ./mc ]; then
-    wget --quiet -O mc https://dl.minio.io/client/mc/release/linux-amd64/mc && \
-	chmod +x mc
+	wget --quiet -O mc https://dl.minio.io/client/mc/release/linux-amd64/mc &&
+		chmod +x mc
 fi
 
 export CI=true
 
-(minio server /tmp/xl/{1...10}/disk{0...1} 2>&1 >/dev/null)&
+(minio server /tmp/xl/{1...10}/disk{0...1} 2>&1 >/dev/null) &
 pid=$!
 
 sleep 2
@@ -47,7 +47,7 @@ user_count=$(./mc admin user list myminio/ | wc -l)
 policy_count=$(./mc admin policy list myminio/ | wc -l)
 
 ## create a warm tier instance
-(minio server /tmp/xltier/{1...4}/disk{0...1} --address :9001 2>&1 >/dev/null)&
+(minio server /tmp/xltier/{1...4}/disk{0...1} --address :9001 2>&1 >/dev/null) &
 sleep 2
 export MC_HOST_mytier="http://minioadmin:minioadmin@localhost:9001/"
 
@@ -58,10 +58,10 @@ export MC_HOST_mytier="http://minioadmin:minioadmin@localhost:9001/"
 ./mc ilm add myminio/bucket2 --transition-days 0 --transition-tier TIER1 --transition-days 0
 ## mirror some content to bucket2 and capture versions tiered
 ./mc mirror internal myminio/bucket2/ --quiet >/dev/null
-./mc ls -r myminio/bucket2/ > bucket2_ns.txt
-./mc ls -r --versions myminio/bucket2/ > bucket2_ns_versions.txt
+./mc ls -r myminio/bucket2/ >bucket2_ns.txt
+./mc ls -r --versions myminio/bucket2/ >bucket2_ns_versions.txt
 sleep 2
-./mc ls -r --versions mytier/tiered/ > tiered_ns_versions.txt
+./mc ls -r --versions mytier/tiered/ >tiered_ns_versions.txt
 
 kill $pid
 (minio server /tmp/xl/{1...10}/disk{0...1} /tmp/xl/{11...30}/disk{0...3} 2>&1 >/tmp/expanded.log) &
@@ -73,38 +73,37 @@ expanded_user_count=$(./mc admin user list myminio/ | wc -l)
 expanded_policy_count=$(./mc admin policy list myminio/ | wc -l)
 
 if [ $user_count -ne $expanded_user_count ]; then
-    echo "BUG: original user count differs from expanded setup"
-    exit 1
+	echo "BUG: original user count differs from expanded setup"
+	exit 1
 fi
 
 if [ $policy_count -ne $expanded_policy_count ]; then
-    echo "BUG: original policy count  differs from expanded setup"
-    exit 1
+	echo "BUG: original policy count  differs from expanded setup"
+	exit 1
 fi
 
 ./mc version info myminio/versioned | grep -q "versioning is enabled"
 ret=$?
 if [ $ret -ne 0 ]; then
-    echo "expected versioning enabled after expansion"
-    exit 1
+	echo "expected versioning enabled after expansion"
+	exit 1
 fi
 
 ./mc mirror cmd myminio/versioned/ --quiet >/dev/null
 
-./mc ls -r myminio/versioned/ > expanded_ns.txt
-./mc ls -r --versions myminio/versioned/ > expanded_ns_versions.txt
+./mc ls -r myminio/versioned/ >expanded_ns.txt
+./mc ls -r --versions myminio/versioned/ >expanded_ns_versions.txt
 
 ./mc admin decom start myminio/ /tmp/xl/{1...10}/disk{0...1}
 
-until $(./mc admin decom status myminio/ | grep -q Complete)
-do
-    echo "waiting for decom to finish..."
-    sleep 1
+until $(./mc admin decom status myminio/ | grep -q Complete); do
+	echo "waiting for decom to finish..."
+	sleep 1
 done
 
 kill $pid
 
-(minio server /tmp/xl/{11...30}/disk{0...3} 2>&1 >/dev/null)&
+(minio server /tmp/xl/{11...30}/disk{0...3} 2>&1 >/dev/null) &
 pid=$!
 
 sleep 2
@@ -113,82 +112,82 @@ decom_user_count=$(./mc admin user list myminio/ | wc -l)
 decom_policy_count=$(./mc admin policy list myminio/ | wc -l)
 
 if [ $user_count -ne $decom_user_count ]; then
-    echo "BUG: original user count differs after decommission"
-    exit 1
+	echo "BUG: original user count differs after decommission"
+	exit 1
 fi
 
 if [ $policy_count -ne $decom_policy_count ]; then
-    echo "BUG: original policy count differs after decommission"
-    exit 1
+	echo "BUG: original policy count differs after decommission"
+	exit 1
 fi
 
 ./mc version info myminio/versioned | grep -q "versioning is enabled"
 ret=$?
 if [ $ret -ne 0 ]; then
-    echo "BUG: expected versioning enabled after decommission"
-    exit 1
+	echo "BUG: expected versioning enabled after decommission"
+	exit 1
 fi
 
-./mc ls -r myminio/versioned > decommissioned_ns.txt
-./mc ls -r --versions myminio/versioned > decommissioned_ns_versions.txt
+./mc ls -r myminio/versioned >decommissioned_ns.txt
+./mc ls -r --versions myminio/versioned >decommissioned_ns_versions.txt
 
 out=$(diff -qpruN expanded_ns.txt decommissioned_ns.txt)
 ret=$?
 if [ $ret -ne 0 ]; then
-    echo "BUG: expected no missing entries after decommission: $out"
-    exit 1
+	echo "BUG: expected no missing entries after decommission: $out"
+	exit 1
 fi
 
 out=$(diff -qpruN expanded_ns_versions.txt decommissioned_ns_versions.txt)
 ret=$?
 if [ $ret -ne 0 ]; then
-    echo "BUG: expected no missing entries after decommission: $out"
-    exit 1
+	echo "BUG: expected no missing entries after decommission: $out"
+	exit 1
 fi
 
 got_checksum=$(./mc cat myminio/versioned/dsync/drwmutex.go | md5sum)
 if [ "${expected_checksum}" != "${got_checksum}" ]; then
-    echo "BUG: decommission failed on encrypted objects: expected ${expected_checksum} got ${got_checksum}"
-    exit 1
+	echo "BUG: decommission failed on encrypted objects: expected ${expected_checksum} got ${got_checksum}"
+	exit 1
 fi
 
 # after decommissioning, compare listings in bucket2 and tiered
 ./mc version info myminio/bucket2 | grep -q "versioning is enabled"
 ret=$?
 if [ $ret -ne 0 ]; then
-    echo "BUG: expected versioning enabled after decommission on bucket2"
-    exit 1
+	echo "BUG: expected versioning enabled after decommission on bucket2"
+	exit 1
 fi
 
-./mc ls -r myminio/bucket2 > decommissioned_bucket2_ns.txt
-./mc ls -r --versions myminio/bucket2 > decommissioned_bucket2_ns_versions.txt
-./mc ls -r --versions mytier/tiered/ > tiered_ns_versions2.txt
+./mc ls -r myminio/bucket2 >decommissioned_bucket2_ns.txt
+./mc ls -r --versions myminio/bucket2 >decommissioned_bucket2_ns_versions.txt
+./mc ls -r --versions mytier/tiered/ >tiered_ns_versions2.txt
 
 out=$(diff -qpruN bucket2_ns.txt decommissioned_bucket2_ns.txt)
 ret=$?
 if [ $ret -ne 0 ]; then
-    echo "BUG: expected no missing entries after decommission in bucket2: $out"
-    exit 1
+	echo "BUG: expected no missing entries after decommission in bucket2: $out"
+	exit 1
 fi
 
 out=$(diff -qpruN bucket2_ns_versions.txt decommissioned_bucket2_ns_versions.txt)
 ret=$?
 if [ $ret -ne 0 ]; then
-    echo "BUG: expected no missing entries after decommission in bucket2x: $out"
-    exit 1
+	echo "BUG: expected no missing entries after decommission in bucket2x: $out"
+	exit 1
 fi
 
 out=$(diff -qpruN tiered_ns_versions.txt tiered_ns_versions2.txt)
 ret=$?
 if [ $ret -ne 0 ]; then
-    echo "BUG: expected no missing entries after decommission in warm tier: $out"
-    exit 1
+	echo "BUG: expected no missing entries after decommission in warm tier: $out"
+	exit 1
 fi
 
 got_checksum=$(./mc cat myminio/bucket2/dsync/drwmutex.go | md5sum)
 if [ "${expected_checksum}" != "${got_checksum}" ]; then
-    echo "BUG: decommission failed on encrypted objects with tiering: expected ${expected_checksum} got ${got_checksum}"
-    exit 1
+	echo "BUG: decommission failed on encrypted objects with tiering: expected ${expected_checksum} got ${got_checksum}"
+	exit 1
 fi
 
 kill $pid
