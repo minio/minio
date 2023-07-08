28a01f03208d1063905006c583088e9b9b31d9c7
Harshavardhana <harsha@minio.io>
2023-07-08 10:42:05 -0700

    update missing license header in files (#17603)

--- internal/http/transports.go
+++ internal/http/transports.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-2022 MinIO, Inc.
 //
-// # This file is part of MinIO Object Storage stack
+// This file is part of MinIO Object Storage stack
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
