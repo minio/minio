b0517df6c4c1e23972b1afc83fb1b4a1df353d3a
guozhi.li <guozhi.li@daocloud.io>
2023-07-19 10:03:23 +0800

    fix by host

--- cmd/metrics-realtime.go
+++ cmd/metrics-realtime.go
@@ -19,6 +19,7 @@ package cmd
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/minio/madmin-go/v3"
@@ -38,8 +39,22 @@ func collectLocalMetrics(types madmin.MetricType, opts collectMetricsOpts) (m ma
 		return
 	}
 
+	byHostName := globalMinioAddr
 	if len(opts.hosts) > 0 {
-		if _, ok := opts.hosts[globalMinioAddr]; !ok {
+		matched := false
+		servers := globalNotificationSys.ServerInfo()
+		server := getLocalServerProperty(globalEndpoints, &http.Request{
+			Host: globalLocalNodeName,
+		})
+		servers = append(servers, server)
+		for _, server := range servers {
+			if _, ok := opts.hosts[server.Endpoint]; ok {
+				byHostName = server.Endpoint
+				matched = true
+				break
+			}
+		}
+		if !matched {
 			return
 		}
 	}
@@ -85,8 +100,8 @@ func collectLocalMetrics(types madmin.MetricType, opts collectMetricsOpts) (m ma
 	// Add types...
 
 	// ByHost is a shallow reference, so careful about sharing.
-	m.ByHost = map[string]madmin.Metrics{globalMinioAddr: m.Aggregated}
-	m.Hosts = append(m.Hosts, globalMinioAddr)
+	m.ByHost = map[string]madmin.Metrics{byHostName: m.Aggregated}
+	m.Hosts = append(m.Hosts, byHostName)
 
 	return m
 }
