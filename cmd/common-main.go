minio/minio

commit 20b79f8945bfd593614590a4d3beb1f0ab783b9f

fix: env depend on the flag (#18231)

cmd/server-main.go
@@ -579,13 +579,13 @@ func serverMain(ctx *cli.Context) {
 
 	setDefaultProfilerRates()
 
-	// Handle all server environment vars.
-	serverHandleEnvVars()
-
 	// Handle all server command args.
 	bootstrapTrace("serverHandleCmdArgs", func() {
 		serverHandleCmdArgs(ctx)
 	})
+
+	// Handle all server environment vars.
+	serverHandleEnvVars()
 
 	// Initialize globalConsoleSys system
 	bootstrapTrace("newConsoleLogger", func() {

commit 1dae6413141bab0f91b5d389cc0302e7039a3a37

fix: always load ENVs from files first as soon as server starts
This is a regression from #18231, however reading from ENV files
must happen well before any parsing logic is invoked.

cmd/common-main.go
@@ -619,8 +619,6 @@ func loadEnvVarsFromFiles() {
 }
 
 func handleCommonEnvVars() {
-	loadEnvVarsFromFiles()
-
 	var err error
 	globalBrowserEnabled, err = config.ParseBool(env.Get(config.EnvBrowser, config.EnableOn))
 	if err != nil {

cmd/server-main.go
@@ -579,6 +579,9 @@ func serverMain(ctx *cli.Context) {
 
 	setDefaultProfilerRates()
 
+	// Always load ENV variables from files first.
+	loadEnvVarsFromFiles()
+
 	// Handle all server command args.
 	bootstrapTrace("serverHandleCmdArgs", func() {
 		serverHandleCmdArgs(ctx)