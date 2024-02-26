30c25965120a3043a6e5d65f88ff917b0f43ff9f
Praveen raj Mani <praveen@minio.io>
2024-02-27 01:04:50 +0530

    Read drive IO stats from sysfs instead of procfs (#19131)
    Currently, we read from `/proc/diskstats` which is found to be
    un-reliable in k8s environments. We can read from `sysfs` instead.
    
    Also, cache the latest drive io stats to find the diff and update
    the metrics.

--- internal/disk/stat_windows.go
+++ internal/disk/stat_windows.go
@@ -108,7 +108,7 @@ func GetInfo(path string, _ bool) (info Info, err error) {
 	return info, nil
 }
 
-// GetAllDrivesIOStats returns IO stats of all drives found in the machine
-func GetAllDrivesIOStats() (info AllDrivesIOStats, err error) {
-	return nil, errors.New("operation unsupported")
+// GetDriveStats returns IO stats of the drive by its major:minor
+func GetDriveStats(major, minor uint32) (iostats IOStats, err error) {
+	return IOStats{}, errors.New("operation unsupported")
 }
