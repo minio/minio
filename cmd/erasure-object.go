95e3b110654147bbe7dd51b0779d7562950957b4
Harshavardhana <harsha@minio.io>
2024-04-16 15:24:43 -0700

    avoid probing tier target while reloading() tier config

--- cmd/erasure-object.go
+++ cmd/erasure-object.go
@@ -2270,7 +2270,7 @@ func (er erasureObjects) GetObjectTags(ctx context.Context, bucket, object strin
 
 // TransitionObject - transition object content to target tier.
 func (er erasureObjects) TransitionObject(ctx context.Context, bucket, object string, opts ObjectOptions) error {
-	tgtClient, err := globalTierConfigMgr.getDriver(opts.Transition.Tier)
+	tgtClient, err := globalTierConfigMgr.getDriver(ctx, opts.Transition.Tier)
 	if err != nil {
 		return err
 	}
