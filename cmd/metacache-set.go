dfc112c06b6d309a1380ed5e14ee2976e5649df9
Anis Eleuch <vadmeste@users.noreply.github.com>
2024-04-18 05:42:11 +0100

    list: Fix rare listing continuation freeze (#19524)
    Reading the list metacache is not protected by a lock; the code retries when it fails
    to read the metacache object, however, it forgot to re-read the metacache object
    from the drives, which is necessary, especially if the metacache object is inlined.
    
    This commit will ensure that we always re-read the metacache object from the drives
    when it is retrying.

--- cmd/metacache-set.go
+++ cmd/metacache-set.go
@@ -573,6 +573,7 @@ func (er *erasureObjects) streamMetadataParts(ctx context.Context, o listPathOpt
 					continue
 				case InsufficientReadQuorum:
 					retries++
+					loadedPart = -1
 					time.Sleep(retryDelay250)
 					continue
 				default:
