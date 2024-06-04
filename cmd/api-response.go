21b62046924114b23b35dab18431d91a20d2c746
Shubhendu <shubhendu@minio.io>
2024-06-04 17:08:26 +0530

    Test proxying of DEL marker for bucket replication (#19870)
    Make sure to avoid proxying for DEL markers
    
    Signed-off-by: Shubhendu Ram Tripathi <shubhendu@minio.io>

--- cmd/api-response.go
+++ cmd/api-response.go
@@ -789,7 +789,7 @@ func generateInitiateMultipartUploadResponse(bucket, key, uploadID string) Initi
 }
 
 // generates CompleteMultipartUploadResponse for given bucket, key, location and ETag.
-func generateCompleteMultpartUploadResponse(bucket, key, location string, oi ObjectInfo) CompleteMultipartUploadResponse {
+func generateCompleteMultipartUploadResponse(bucket, key, location string, oi ObjectInfo) CompleteMultipartUploadResponse {
 	cs := oi.decryptChecksums(0)
 	c := CompleteMultipartUploadResponse{
 		Location: location,
