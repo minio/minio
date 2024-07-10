1f255b236cb60b7757ce992f6f969c33f844a857
Anis Eleuch <anis@min.io>
2024-07-10 16:39:20 +0100

    Address comment review

--- cmd/global-heal.go
+++ cmd/global-heal.go
@@ -519,9 +519,7 @@ func (er *erasureObjects) healErasureSet(ctx context.Context, buckets []string, 
 				go healEntry(bucket, *entry)
 			},
 			finished: func(errs []error) {
-				found := false
-				found := countErrs(errs, nil) != len(errs)
-				if found {
+				if countErrs(errs, nil) != len(errs) {
 					retErr = fmt.Errorf("one or more errors reported during listing: %v", errors.Join(errs...))
 				}
 			},
