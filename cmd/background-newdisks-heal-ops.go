2d7cec8ed284b5a6111c75b4116ee5076d3fd100
Harshavardhana <harsha@minio.io>
2024-08-23 13:42:22 -0700

    to turn-off healing drop legacy ENV

--- cmd/background-newdisks-heal-ops.go
+++ cmd/background-newdisks-heal-ops.go
@@ -384,8 +384,7 @@ func initAutoHeal(ctx context.Context, objAPI ObjectLayer) {
 	}
 
 	initBackgroundHealing(ctx, objAPI) // start quick background healing
-
-	if env.Get("_MINIO_AUTO_DRIVE_HEALING", config.EnableOn) == config.EnableOn || env.Get("_MINIO_AUTO_DISK_HEALING", config.EnableOn) == config.EnableOn {
+	if env.Get("_MINIO_AUTO_DRIVE_HEALING", config.EnableOn) == config.EnableOn {
 		globalBackgroundHealState.pushHealLocalDisks(getLocalDisksToHeal()...)
 		go monitorLocalDisksAndHeal(ctx, z)
 	}
