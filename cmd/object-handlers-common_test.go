3bdc473d22796d5656a433056350c99acd610631
Tangrunze <614560331@qq.com>
2024-08-27 14:36:40 +0800

    s3: Modify unit test

--- cmd/object-handlers-common_test.go
+++ cmd/object-handlers-common_test.go
@@ -62,43 +62,72 @@ func TestCanonicalizeETag(t *testing.T) {
 // Tests - CheckPreconditions()
 func TestCheckPreconditions(t *testing.T) {
 	objModTime := time.Date(2024, 8, 26, 0o2, 0o1, 0o1, 0, time.UTC)
-	testCases := []struct {
-		name              string
-		ifMatch           string
-		ifNoneMatch       string
-		ifModifiedSince   string
-		ifUnmodifiedSince string
-		objInfo           ObjectInfo
-		expectedFlag      bool
-		expectedCode      int
-	}{
-		// If-None-Match(false) and If-Modified-Since(true)
-		{name: "If-None-Match1", ifNoneMatch: "aa", ifModifiedSince: "Sun, 26 Aug 2024 02:01:00 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: true, expectedCode: 304},
-		// If-Modified-Since(false)
-		{name: "If-None-Match2", ifNoneMatch: "aaa", ifModifiedSince: "Sun, 26 Aug 2024 02:01:01 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: true, expectedCode: 304},
-		{name: "If-None-Match3", ifNoneMatch: "aaa", ifModifiedSince: "Sun, 26 Aug 2024 02:01:02 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: true, expectedCode: 304},
-
-		// If-Match(true) and If-Unmodified-Since(false)
-		{name: "If-Match1", ifMatch: "aa", ifUnmodifiedSince: "Sun, 26 Aug 2024 02:01:00 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: false, expectedCode: 200},
-		// If-Unmodified-Since(true)
-		{name: "If-Match2", ifMatch: "aa", ifUnmodifiedSince: "Sun, 26 Aug 2024 02:01:01 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: false, expectedCode: 200},
-		{name: "If-Match3", ifMatch: "aa", ifUnmodifiedSince: "Sun, 26 Aug 2024 02:01:02 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: false, expectedCode: 200},
-		// If-Match(true)
-		{name: "If-Match4", ifMatch: "aa", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: false, expectedCode: 200},
-	}
-	for _, tc := range testCases {
-		recorder := httptest.NewRecorder()
-		request := httptest.NewRequest(http.MethodHead, "/bucket/a", bytes.NewReader([]byte{}))
-		request.Header.Set(xhttp.IfNoneMatch, tc.ifNoneMatch)
-		request.Header.Set(xhttp.IfModifiedSince, tc.ifModifiedSince)
-		request.Header.Set(xhttp.IfMatch, tc.ifMatch)
-		request.Header.Set(xhttp.IfUnmodifiedSince, tc.ifUnmodifiedSince)
-		actualFlag := checkPreconditions(context.Background(), recorder, request, tc.objInfo, ObjectOptions{})
-		if tc.expectedFlag != actualFlag {
-			t.Errorf("test: %s, got flag: %v, want: %v", tc.name, actualFlag, tc.expectedFlag)
+	t.Run("If-None-Match", func(t *testing.T) {
+		testCases := []struct {
+			name              string
+			ifMatch           string
+			ifNoneMatch       string
+			ifModifiedSince   string
+			ifUnmodifiedSince string
+			objInfo           ObjectInfo
+			expectedFlag      bool
+			expectedCode      int
+		}{
+			// If-None-Match(false) and If-Modified-Since(true)
+			{name: "If-None-Match1", ifNoneMatch: "aa", ifModifiedSince: "Sun, 26 Aug 2024 02:01:00 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: true, expectedCode: 304},
+			// If-Modified-Since(false)
+			{name: "If-None-Match2", ifNoneMatch: "aaa", ifModifiedSince: "Sun, 26 Aug 2024 02:01:01 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: true, expectedCode: 304},
+			{name: "If-None-Match3", ifNoneMatch: "aaa", ifModifiedSince: "Sun, 26 Aug 2024 02:01:02 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: true, expectedCode: 304},
 		}
-		if tc.expectedCode != recorder.Code {
-			t.Errorf("test: %s, got code: %d, want: %d", tc.name, recorder.Code, tc.expectedCode)
+		for _, tc := range testCases {
+			recorder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodHead, "/bucket/a", bytes.NewReader([]byte{}))
+			request.Header.Set(xhttp.IfNoneMatch, tc.ifNoneMatch)
+			request.Header.Set(xhttp.IfModifiedSince, tc.ifModifiedSince)
+			request.Header.Set(xhttp.IfMatch, tc.ifMatch)
+			request.Header.Set(xhttp.IfUnmodifiedSince, tc.ifUnmodifiedSince)
+			actualFlag := checkPreconditions(context.Background(), recorder, request, tc.objInfo, ObjectOptions{})
+			if tc.expectedFlag != actualFlag {
+				t.Errorf("test: %s, got flag: %v, want: %v", tc.name, actualFlag, tc.expectedFlag)
+			}
+			if tc.expectedCode != recorder.Code {
+				t.Errorf("test: %s, got code: %d, want: %d", tc.name, recorder.Code, tc.expectedCode)
+			}
 		}
-	}
+	})
+	t.Run("If-Match", func(t *testing.T) {
+		testCases := []struct {
+			name              string
+			ifMatch           string
+			ifNoneMatch       string
+			ifModifiedSince   string
+			ifUnmodifiedSince string
+			objInfo           ObjectInfo
+			expectedFlag      bool
+			expectedCode      int
+		}{
+			// If-Match(true) and If-Unmodified-Since(false)
+			{name: "If-Match1", ifMatch: "aa", ifUnmodifiedSince: "Sun, 26 Aug 2024 02:01:00 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: false, expectedCode: 200},
+			// If-Unmodified-Since(true)
+			{name: "If-Match2", ifMatch: "aa", ifUnmodifiedSince: "Sun, 26 Aug 2024 02:01:01 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: false, expectedCode: 200},
+			{name: "If-Match3", ifMatch: "aa", ifUnmodifiedSince: "Sun, 26 Aug 2024 02:01:02 GMT", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: false, expectedCode: 200},
+			// If-Match(true)
+			{name: "If-Match4", ifMatch: "aa", objInfo: ObjectInfo{ETag: "aa", ModTime: objModTime}, expectedFlag: false, expectedCode: 200},
+		}
+		for _, tc := range testCases {
+			recorder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodHead, "/bucket/a", bytes.NewReader([]byte{}))
+			request.Header.Set(xhttp.IfNoneMatch, tc.ifNoneMatch)
+			request.Header.Set(xhttp.IfModifiedSince, tc.ifModifiedSince)
+			request.Header.Set(xhttp.IfMatch, tc.ifMatch)
+			request.Header.Set(xhttp.IfUnmodifiedSince, tc.ifUnmodifiedSince)
+			actualFlag := checkPreconditions(context.Background(), recorder, request, tc.objInfo, ObjectOptions{})
+			if tc.expectedFlag != actualFlag {
+				t.Errorf("test: %s, got flag: %v, want: %v", tc.name, actualFlag, tc.expectedFlag)
+			}
+			if tc.expectedCode != recorder.Code {
+				t.Errorf("test: %s, got code: %d, want: %d", tc.name, recorder.Code, tc.expectedCode)
+			}
+		}
+	})
 }
