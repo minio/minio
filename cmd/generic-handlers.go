c07e5b49d477b0774f23db3b290745aef8c01bd2
Eng Zer Jun <engzerjun@gmail.com>
2024-11-26 01:10:22 +0800

    refactor: replace experimental `maps` and `slices` with stdlib (#20679)
    The experimental functions are now available in the standard library in
    Go 1.23 [1].
    
    [1]: https://go.dev/doc/go1.23#new-unique-package
    
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

--- cmd/generic-handlers.go
+++ cmd/generic-handlers.go
@@ -33,8 +33,6 @@ import (
 	"github.com/minio/minio-go/v7/pkg/set"
 	"github.com/minio/minio/internal/grid"
 	xnet "github.com/minio/pkg/v3/net"
-	"golang.org/x/exp/maps"
-	"golang.org/x/exp/slices"
 
 	"github.com/minio/minio/internal/amztime"
 	"github.com/minio/minio/internal/config/dns"
@@ -75,7 +73,7 @@ const (
 // and must not set by clients
 func containsReservedMetadata(header http.Header) bool {
 	for key := range header {
-		if slices.Contains(maps.Keys(validSSEReplicationHeaders), key) {
+		if _, ok := validSSEReplicationHeaders[key]; ok {
 			return false
 		}
 		if stringsHasPrefixFold(key, ReservedMetadataPrefix) {
