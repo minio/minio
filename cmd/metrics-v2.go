35d14c698f2181857a3bf81359afa131bcc08b74
buddhi.poudel <buddhi.poudel@extensodata.com>
2025-03-03 17:10:42 +0545

    Fixed: added bucket in makedescription

--- cmd/metrics-v2.go
+++ cmd/metrics-v2.go
@@ -319,6 +319,7 @@ type MetricDescription struct {
 	Name      MetricName      `json:"MetricName"`
 	Help      string          `json:"Help"`
 	Type      MetricTypeV2    `json:"Type"`
+	Buckets   []float64       `json:"Buckets,omitempty"` 
 }
 
 // MetricV2 captures the details for a metric
@@ -1544,7 +1545,7 @@ func getS3TTFBDistributionMD() MetricDescription {
 		Name:      ttfbDistribution,
 		Help:      "Distribution of time to first byte across API calls",
 		Type:      histogramMetric, 
-		Buckets:   []float64{0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0},
+		Buckets:   []float64{0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0}
 	}
 }
 
@@ -1555,7 +1556,7 @@ func getBucketTTFBDistributionMD() MetricDescription {
 		Name:      ttfbDistribution,
 		Help:      "Distribution of time to first byte across API calls per bucket",
 		Type:      histogramMetric, 
-		Buckets:   []float64{0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0}, 
+		Buckets:   []float64{0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0}
 	}
 }
 
