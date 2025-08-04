d2a6e76192f39049e66bc9833ee1fb39e149c05d
M Alvee <alvee.developer@gmail.com>
2025-08-04 22:28:45 +0600

    Update useragent.go

--- internal/useragent/useragent.go
+++ internal/useragent/useragent.go
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-2025 MinIO, Inc.
 //
-// This file is part of MinIO Object Storage stack
+// # This file is part of MinIO Object Storage stack
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,6 +14,7 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package useragent
 
 import (
