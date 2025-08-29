dd749005cdc226d7c1352b3796d14bb1ea76996f
M Alvee <alvee.developer@gmail.com>
2025-08-29 18:37:06 +0600

    Update erasure-object.go

--- cmd/erasure-object.go
+++ cmd/erasure-object.go
@@ -1274,8 +1274,13 @@ func (er erasureObjects) putObject(ctx context.Context, bucket string, object st
 		if err == nil && opts.CheckPrecondFn(obj) {
 			return objInfo, PreConditionFailed{}
 		}
-		if err != nil && !isErrReadQuorum(err) {
-			return objInfo, err
+		if err != nil {
+			if opts.ReplicationRequest && !(isErrVersionNotFound(err) || isErrObjectNotFound(err) || isErrReadQuorum(err)) {
+				return objInfo, err
+			}
+			if !opts.ReplicationRequest && !isErrReadQuorum(err) {
+				return objInfo, err
+			}
 		}
 	}
 
