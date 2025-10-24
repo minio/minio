cd5638af8c7d7df18462165ff6242aa1516b820b
Joakim Hindersson <joakim.hindersson@elastx.se>
2025-10-24 09:31:40 +0200

    Fix build script for elx

--- docker-buildx.sh
+++ docker-buildx.sh
@@ -3,8 +3,8 @@
 sudo sysctl net.ipv6.conf.all.disable_ipv6=0
 
 remote=$(git remote get-url upstream)
-if test "$remote" != "git@github.com:minio/minio.git"; then
-	echo "Script requires that the 'upstream' remote is set to git@github.com:minio/minio.git"
+if test "$remote" != "git@github.com:elastx/minio.git"; then
+	echo "Script requires that the 'upstream' remote is set to git@github.com:elastx/minio.git"
 	exit 1
 fi
 
@@ -14,12 +14,12 @@ release=$(git describe --abbrev=0 --tags)
 
 docker buildx build --push --no-cache \
 	--build-arg RELEASE="${release}" \
-	-t "minio/minio:latest" \
-	-t "minio/minio:latest-cicd" \
-	-t "quay.io/minio/minio:latest" \
-	-t "quay.io/minio/minio:latest-cicd" \
-	-t "minio/minio:${release}" \
-	-t "quay.io/minio/minio:${release}" \
+	-t "elastx/minio:latest" \
+	-t "elastx/minio:latest-cicd" \
+	-t "quay.io/elastx/minio:latest" \
+	-t "quay.io/elastx/minio:latest-cicd" \
+	-t "elastx/minio:${release}" \
+	-t "quay.io/elastx/minio:${release}" \
 	--platform=linux/arm64,linux/amd64,linux/ppc64le \
 	-f Dockerfile.release .
 
